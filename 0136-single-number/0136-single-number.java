056ac061b0cf70ec5a859e7c85184c1c3be2f993
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-08-15 00:23:24 -0400

    Time: 1 ms (100%), Space: 44.3 MB (74.43%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int mask = 0;
+        for (int num : nums){
+            mask ^= num;
+        }
+
+        return mask;
+    }
+}

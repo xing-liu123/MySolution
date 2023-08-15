e7bec5f75042bb5adf4ec674c1f667d4e342656e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-08-15 19:25:28 -0400

    Time: 15 ms (72.01%), Space: 16.6 MB (44.74%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int nextCover = 0;
+        int currCover = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            nextCover = max(nextCover, nums[i] + i);
+            if (i == currCover) {
+                currCover = nextCover;
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+};

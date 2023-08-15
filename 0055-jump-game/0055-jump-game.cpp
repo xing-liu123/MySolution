c7be91943213f9eefbcae7aa69c2859dc7b74be9
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-08-15 19:11:27 -0400

    Time: 49 ms (85.14%), Space: 48.3 MB (74.28%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int range = 0;
+        int i = 0;
+        while (i <= range) {
+            range = max(range, i + nums[i]);
+            if (range >= nums.size() - 1) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+};

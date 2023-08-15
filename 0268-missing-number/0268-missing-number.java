163dd0d76490d3f6bf045e8b57cb09d2a99fbf7b
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-08-15 01:53:12 -0400

    Time: 0 ms (100%), Space: 44.1 MB (71.4%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+            res ^= i;
+        }
+
+        return res;
+        
+    }
+}

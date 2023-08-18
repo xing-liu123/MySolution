00e0970ec6703560b3f54e9be0ad6484150d1cfd
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-08-17 22:21:34 -0400

    Time: 0 ms (100%), Space: 6.4 MB (53.42%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> list;
+    int sum;
+    vector<vector<int>> combinationSum3(int k, int n) {
+        backtracking(1, n, k);
+        return res;
+    }
+
+    void backtracking(int start, int n, int k) {
+        if (list.size() == k && sum == n) {
+            vector<int> copy = list;
+            res.push_back(copy);
+            return;
+        }
+
+        for (int i = start; i <= 9; i++) {
+            if (sum + i > n) {
+                continue;
+            }
+
+            list.push_back(i);
+            sum += i;
+            backtracking(i + 1, n, k);
+            sum -= i;
+            list.pop_back();
+        }
+    }
+};

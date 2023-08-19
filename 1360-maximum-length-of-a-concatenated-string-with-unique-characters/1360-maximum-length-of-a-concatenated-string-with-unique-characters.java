8b3a6a49f605fefd56d2d29d66749e3ee3c36eb8
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-08-19 03:49:41 -0400

    Time: 109 ms (19.31%), Space: 43.8 MB (45.79%) - LeetHub

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -0,0 +1,60 @@
+class Solution {
+    int len = 0;
+    int[] chars = new int[26];
+    int longest = 0;
+    HashSet<Character> set = new HashSet<>();
+    public int maxLength(List<String> arr) {
+        backtrack(arr, 0);
+        return longest;
+    }
+
+    private void backtrack(List<String> arr, int idx) {
+        System.out.println(len);
+        if (len > longest) {
+            longest = len;
+        }
+        if (idx == arr.size()) {
+            
+            return;
+        }
+
+        for (int i = idx; i < arr.size(); i++) {
+            String s = arr.get(i);
+            if (!contains(s) && !duplicate(s)) {
+                for (int j = 0; j < s.length(); j++) {
+                    chars[s.charAt(j) - 'a']++;
+                }
+                len += s.length();
+                backtrack(arr, i + 1);
+                len -= s.length();
+                for (int j = 0; j < s.length(); j++) {
+                    chars[s.charAt(j) - 'a']--;
+                }
+            }
+        }
+    }
+
+    private boolean contains(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (chars[s.charAt(i) - 'a'] > 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean duplicate(String s) {
+         char[] str = s.toCharArray();
+         for(char c : str) {
+             if (!set.add(c)) {
+                 set.clear();
+                 return true;
+             }
+         }
+
+        set.clear();
+        return false;
+
+    }
+}

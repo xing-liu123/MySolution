0fe2fc4b6828ffe373e50f2893c7727c82492045
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-08-20 00:53:52 -0400

    Time: 0 ms (100%), Space: 40.5 MB (56.03%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,54 +1,81 @@
 class Solution {
     public boolean checkValidString(String s) {
         char[] str = s.toCharArray();
-        int count = 0;
-        Stack<Character> stack = new Stack<>();
+        int leftCount = 0, rightCount = 0;
 
         for (int i = 0; i < str.length; i++) {
-            char c = str[i];
-
-            if (c == '(') {
-                stack.push(c);
-            } else if (c == ')') {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                } else if (count > 0) {
-                    count--;
-                } else {
-                    return false;
-                }
+            if (str[i] == '(' || str[i] == '*') {
+                leftCount++;
             } else {
-                count++;
+                leftCount--;
             }
 
+            if (leftCount < 0) {
+                return false;
+            }
         }
-
-        if (count < stack.size()) {
-            return false;
-        }
-
-        int count1 = 0;
-        Stack<Character> stack1 = new Stack<>();
 
         for (int i = str.length - 1; i >= 0; i--) {
-            char c = str[i];
-
-            if (c == ')') {
-                stack1.push(c);
-            } else if (c == '(') {
-                if (!stack1.isEmpty()) {
-                    stack1.pop();
-                } else if (count1 > 0) {
-                    count1--;
-                } else {
-                    return false;
-                }
+            if (str[i] == ')' || str[i] == '*') {
+                rightCount++;
             } else {
-                count1++;
+                rightCount--;
             }
 
+            if (rightCount < 0) {
+                return false;
+            }
         }
 
-        return count1 >= stack1.size();
+        return true;
+        // int count = 0;
+        // Stack<Character> stack = new Stack<>();
+
+        // for (int i = 0; i < str.length; i++) {
+        //     char c = str[i];
+
+        //     if (c == '(') {
+        //         stack.push(c);
+        //     } else if (c == ')') {
+        //         if (!stack.isEmpty()) {
+        //             stack.pop();
+        //         } else if (count > 0) {
+        //             count--;
+        //         } else {
+        //             return false;
+        //         }
+        //     } else {
+        //         count++;
+        //     }
+
+        // }
+
+        // if (count < stack.size()) {
+        //     return false;
+        // }
+
+        // int count1 = 0;
+        // Stack<Character> stack1 = new Stack<>();
+
+        // for (int i = str.length - 1; i >= 0; i--) {
+        //     char c = str[i];
+
+        //     if (c == ')') {
+        //         stack1.push(c);
+        //     } else if (c == '(') {
+        //         if (!stack1.isEmpty()) {
+        //             stack1.pop();
+        //         } else if (count1 > 0) {
+        //             count1--;
+        //         } else {
+        //             return false;
+        //         }
+        //     } else {
+        //         count1++;
+        //     }
+
+        // }
+
+        // return count1 >= stack1.size();
     }
 }

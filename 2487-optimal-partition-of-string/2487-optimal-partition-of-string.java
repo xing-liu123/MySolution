e57f98296a0c38e09993deabddb586b468ea766b
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-08-23 12:21:50 -0400

    Time: 33 ms (56.83%), Space: 44.3 MB (68.94%) - LeetHub

---
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int partitionString(String s) {
+        Set<Character> set = new HashSet<>();
+        int count = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                count++;
+                set.clear();
+            }
+            set.add(s.charAt(i));
+        }
+
+        return count;
+    }
+}

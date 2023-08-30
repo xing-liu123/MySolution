87c823a45764beb7e799500f2a7c5ed069c5a05e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-08-30 16:37:55 -0400

    Time: 3 ms (37.57%), Space: 7.5 MB (95.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,15 +6,17 @@ public:
         if (n == 1) {
             return nums[0];
         }
-        vector<int> earns(n, 0);
 
-        earns[0] = nums[0];
-        earns[1] = max(nums[0], nums[1]);
+
+        int x = nums[0];
+        int y = max(nums[0], nums[1]);
 
         for (int i = 2; i < n; i++) {
-            earns[i] = max(earns[i - 1], earns[i - 2] + nums[i]);
+            int z = max(y, x + nums[i]);
+            x = y;
+            y = z;
         }
 
-        return earns[n - 1];
+        return y;
     }
 };

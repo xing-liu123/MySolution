466fdfd30a43103156e315084b56a2db631fdf01
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-09-04 13:48:56 -0400

    Time: 3 ms (81.08%), Space: 47.3 MB (83.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,41 +1,35 @@
 class Solution {
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
     public int numIslands(char[][] grid) {
 
         int count = 0;
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
+        
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    
                     count++;
-                    grid[i][j] = '0';
 
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[]{i, j});
-                    while (!queue.isEmpty()) {
-                        int[] point = queue.poll();
-
-                        int row = point[0];
-                        int col = point[1];
-
-                        for (int k = 0; k < 4; k++) {
-                            int newRow = row + dr[k];
-                            int newCol = col + dc[k];
-
-                            if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && grid[newRow][newCol] == '1') {
-                                queue.offer(new int[]{newRow, newCol});
-                                grid[newRow][newCol] = '0';
-                            }
-                            
-                        }
-                    }
+                    dfs(grid, i, j);
+                   
                 }
                 
             }
         }
 
         return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        for (int k = 0; k < 4; k++) {
+            dfs(grid, i + dr[k], j + dc[k]);
+        }
     }
 }

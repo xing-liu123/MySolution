5c1143415b495fb38f942005eefb57f8ba672a85
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-09-08 23:04:16 -0400

    Time: 37 ms (10.68%), Space: 46.5 MB (72.6%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,40 +1,51 @@
 class Solution {
+    Map<Integer, List<int[]>> adj;
     public int networkDelayTime(int[][] times, int n, int k) {
-        Map<Integer, List<int[]>> map = new HashMap<>();
+        adj = new HashMap<>();
 
         for (int[] time : times) {
-            map.computeIfAbsent(time[0], x -> new ArrayList<>()).add(new int[]{time[1], time[2]});
+            int source = time[0];
+            int dest = time[1];
+            int dist = time[2];
+
+            adj.putIfAbsent(source, new ArrayList<>());
+            adj.get(source).add(new int[]{dest, dist});
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-
-
-        Map<Integer, Integer> res = new HashMap<>();
-
-        pq.offer(new int[]{k, 0});
-
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int node = curr[0];
-            int time = curr[1];
-
-            if (res.containsKey(node)) {
-                continue;
-            }
-
-            res.put(node, time);
-
-            if (map.containsKey(node)) {
-                for (int[] edge : map.get(node)) {
-                    int nextNode = edge[0];
-                    int newTime = edge[1] + time;
-                    pq.offer(new int[]{nextNode, newTime});
-                }
-                
-            }
+        for (int node : adj.keySet()) {
+            Collections.sort(adj.get(node), (a, b) -> a[1] - b[1]);
         }
 
-        int maxTime = Collections.max(res.values());
-        return res.size() == n ? maxTime : -1;
+        int[] signalReceiveAt = new int[n + 1];
+        Arrays.fill(signalReceiveAt, Integer.MAX_VALUE);
+
+        dfs(signalReceiveAt, k, 0);
+
+        int res = 0;
+
+        for (int i = 1; i <= n; i++) {
+            res = Math.max(res, signalReceiveAt[i]);
+        }
+
+        return res == Integer.MAX_VALUE ? -1 : res;
+
+    }
+
+    private void dfs(int[] signalReceiveAt, int currNode, int currDist) {
+        if (currDist >= signalReceiveAt[currNode]) {
+            return;
+        }
+
+        signalReceiveAt[currNode] = currDist;
+
+        if (!adj.containsKey(currNode)) {
+            return;
+        }
+
+        for (int[] pair : adj.get(currNode)) {
+            int nextNode = pair[0];
+            int newDist = pair[1] + currDist;
+            dfs(signalReceiveAt, nextNode, newDist);
+        }
     }
 }

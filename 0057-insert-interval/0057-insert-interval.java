c2a95917a93a128654ea80afe211c29b4baf236f
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-09-10 00:09:04 -0400

    Time: 1 ms (98.93%), Space: 43.9 MB (51.92%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,63 +1,26 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int n = intervals.length;
+        List<int[]> res = new ArrayList<>();
+        int i = 0,  n = intervals.length;
 
-        if (n == 0) {
-             int[][] res = new int[1][2];
-             res[0] = newInterval;
-            return res;
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
         }
 
-        int left = newInterval[0];
-        int right = newInterval[1];
-
-        if (right < intervals[0][0]) {
-            int[][] res = new int[n + 1][2];
-            res[0] = newInterval;
-            copy(res, intervals, 1, n, 0, n - 1);
-            return res;
-        } else if (right == intervals[0][0] || right <= intervals[0][1]) {
-            intervals[0][0] = Math.min(intervals[0][0], left);
-            return intervals;
-        } else if (left > intervals[n - 1][1]) {
-            int[][] res = new int[n + 1][2];
-            res[n] = newInterval;
-            copy(res, intervals, 0, n - 1, 0, n - 1);
-            return res;
-        } else if (left == intervals[n - 1][1] || left >= intervals[n - 1][0]) {
-            intervals[n - 1][1] = Math.max(intervals[n - 1][1], right);
-            return intervals;
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
         }
 
-        int left_idx = -1;
-        int right_idx = -1;
+        res.add(newInterval);
 
-        for (int i = 0; i < n; i++) {
-            if (intervals[i][1] >= left) {
-                left_idx = i;
-                break;
-            }
+        while(i < n) {
+            res.add(intervals[i]);
+            i++;
         }
 
-        for (int i = n - 1; i >= 0; i--) {
-            if (intervals[i][0] <= right) {
-                right_idx = i;
-                break;
-            }
-        }
-
-        int[][] res = new int[n - (right_idx - left_idx)][2];
-        res[left_idx][0] = Math.min(intervals[left_idx][0], left); 
-        res[left_idx][1] = Math.max(intervals[right_idx][1], right); 
-        copy(res, intervals, 0, left_idx - 1, 0, left_idx - 1);
-        copy(res, intervals, left_idx + 1, n - (right_idx - left_idx) - 1, right_idx + 1, n - 1);
-        return res;
-        
-    }
-
-    private void copy(int[][] newArr, int[][] orgin, int start1, int end1, int start2, int end2) {
-        while (start1 <= end1) {
-            newArr[start1++] = orgin[start2++];
-        }
+        return res.toArray(new int[res.size()][2]);
     }
 }

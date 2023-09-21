fafe35d2fa5c34599a12612e0cd2c0a03aef524d
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-09-21 17:58:55 -0400

    Time: 32 ms (89.67%), Space: 16.3 MB (60.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -8,6 +8,8 @@ class Solution:
         dp[1] = 1
 
         for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+            for j in range(1, 3):
+                if i >= j:
+                    dp[i] += dp[i - j]
         
         return dp[n]

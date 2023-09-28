518f7820e88e8645845b28b03dc2776083302938
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-09-27 22:33:56 -0400

    Time: 384 ms (69.23%), Space: 72.7 MB (61.76%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -8,7 +8,6 @@ class Solution:
         for i in range(m + 1):
             dp[i][0] = 1
 
-
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if s[i - 1] == t[j - 1]:

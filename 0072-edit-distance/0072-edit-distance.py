6b8808450a33757e64bb1d90e2daae61c97fccb8
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-09-28 15:38:22 -0400

    Time: 139 ms (25.67%), Space: 20 MB (60.41%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,27 +1,22 @@
-class Solution(object):
-    def minDistance(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        len1 = len(word1)
-        len2 = len(word2)
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
 
-        dp = [[0] * (len2 + 1) for _ in range(len1 + 1)]
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-        for i in range(len1 + 1):
+        for i in range(1, m + 1):
             dp[i][0] = i
-
-        for j in range(len2 + 1):
-            dp[0][j] = j
         
-        for i in range(1, len1 + 1):
-            for j in range(1, len2 + 1):
+        for j in range(1, n + 1):
+            dp[0][j] = j
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
                 if word1[i - 1] == word2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    dp[i][j] = min(dp[i][j - 1] + 1, min(dp[i - 1][j] + 1, dp[i - 1][j - 1] + 1))
+                    dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1)
         
-
-        return dp[len1][len2]
+        return dp[m][n]
+                

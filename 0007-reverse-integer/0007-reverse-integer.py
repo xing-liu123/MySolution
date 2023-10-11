57ea0b8fff727c0018e3add7cb31dd415bcaddbb
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-10-10 22:50:09 -0400

    Time: 45 ms (22.26%), Space: 16.1 MB (79.72%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = 0
+        maxValue = 2**31 - 1
+        minValue = -2**31
+ 
+        while x != 0:
+            digit = int(math.fmod(x, 10)) 
+            
+            if res > maxValue // 10 or (res == maxValue // 10 and digit > maxValue % 10):
+                return 0
+            
+            if res < minValue // 10 or (res == minValue // 10 and digit < minValue % 10):
+                return 0
+            
+            res = res * 10 + digit
+            x = int(x / 10)
+
+        return res

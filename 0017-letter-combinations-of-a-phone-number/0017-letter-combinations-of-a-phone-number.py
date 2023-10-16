69b4ec5d8345540eb4aa8a06a131cd6ab8691ac3
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-10-16 19:50:39 -0400

    Time: 40 ms (45.19%), Space: 16.1 MB (98.86%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def __init__(self):
+        self.path = ""
+        self.res = []
+        self.map = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]] 
+
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return self.res
+            
+        self.backtracking(digits, 0)
+        return self.res
+    
+    def backtracking(self, digits: str, idx: int):
+        if idx == len(digits):
+            self.res.append(copy.copy(self.path))
+            return
+
+        keys = self.map[int(digits[idx]) - 2]
+
+        for i in range(len(keys)):
+            self.path += keys[i]
+            self.backtracking(digits, idx + 1)
+            self.path = self.path[:len(self.path) - 1]

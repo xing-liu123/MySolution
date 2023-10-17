5ff94231e5420ae2c88b4231ccd5359c49c11de1
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-10-16 22:38:45 -0400

    Time: 6658 ms (5.84%), Space: 16.2 MB (78.31%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -0,0 +1,39 @@
+class Solution:
+    def __init__(self):
+        self.sums = []
+        self.target = 0
+    
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        totalSum = sum(matchsticks)
+
+        if totalSum % 4 != 0:
+            return False
+
+        matchsticks.sort(reverse=True)
+
+        self.sums = [0] * 4
+
+        self.target = totalSum//4
+
+        return self.backtracking(matchsticks, 0)
+
+    
+    def backtracking(self, matchsticks: List[int], idx: int) -> bool:
+        if idx == len(matchsticks):
+            return self.sums[0] == self.target and self.sums[1] == self.target and self.sums[2] == self.target and self.sums[3] == self.target
+               
+        if idx < len(matchsticks):
+            if matchsticks[idx] > self.target:
+                return False
+
+            for i in range(4):
+                if self.sums[i] + matchsticks[idx] > self.target:
+                    continue
+                self.sums[i] += matchsticks[idx]
+                if self.backtracking(matchsticks, idx + 1):
+                    return True
+                self.sums[i] -= matchsticks[idx]
+
+            
+        return False
+        

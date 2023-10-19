17124905482c078fa3fe1321c97345599123df92
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-10-19 19:21:17 -0400

    Time: 87 ms (21.24%), Space: 16.5 MB (16.56%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,44 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = []
+        board = [['.'] * n for _ in range(n)]
+
+        def backtrack(row: int):
+            if row == n:
+                res.append([''.join(row) for row in board])
+                return
+            
+            for col in range(n):
+                if isValid(row, col):
+                    board[row][col] = 'Q'
+                    backtrack(row + 1)
+                    board[row][col] = '.'
+        
+        def isValid(row: int, col: int) -> bool:
+            for i in range(0, row):
+                if board[i][col] == 'Q':
+                    return False
+            
+            i = row - 1
+            j = col - 1
+
+            while i >= 0 and j >= 0:
+                if board[i][j] == 'Q':
+                    return False
+                i -= 1
+                j -= 1
+            
+            i = row - 1
+            j = col + 1
+
+            while i >= 0 and j < n:
+                if board[i][j] == 'Q':
+                    return False
+                i -= 1
+                j += 1
+            
+            return True
+        
+        backtrack(0)
+
+        return len(res)

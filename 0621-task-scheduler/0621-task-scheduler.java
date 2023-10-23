d80c0fd1052411f8baa9123ffff45feb71e581e3
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-10-23 11:14:24 -0400

    Time: 2 ms (97.84%), Space: 43.8 MB (95.97%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,42 +1,61 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        Map<Character, Integer> freq = new HashMap<>();
+        // Map<Character, Integer> freq = new HashMap<>();
 
-        for (char task : tasks) {
-            freq.put(task, freq.getOrDefault(task, 0) + 1);
+        // for (char task : tasks) {
+        //     freq.put(task, freq.getOrDefault(task, 0) + 1);
+        // }
+
+        // PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> freq.get(b) - freq.get(a));
+
+        // pq.addAll(freq.keySet());
+
+        // int totalInterval = 0;
+
+        // while (!pq.isEmpty()) {
+        //     List<Character> coolDownList = new ArrayList<>();
+
+        //     int interval = 0;
+
+        //     while (interval <= n) {
+        //         if (!pq.isEmpty()) {
+        //             char c = pq.poll();
+        //             freq.put(c, freq.get(c) - 1);
+
+        //             if (freq.get(c) > 0) {
+        //                 coolDownList.add(c);
+        //             }
+        //         }
+        //         interval++;
+        //         totalInterval++;
+
+        //         if (pq.isEmpty() && coolDownList.isEmpty()) {
+        //             return totalInterval;
+        //         }
+        //     }
+
+        //     pq.addAll(coolDownList);
+        // }
+
+        // return totalInterval;
+
+        int[] freq = new int[26];
+
+        for (char c : tasks) {
+            freq[c - 'A']++; 
         }
 
-        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> freq.get(b) - freq.get(a));
+        Arrays.sort(freq);
 
-        pq.addAll(freq.keySet());
+        int freq_max = freq[freq.length - 1];
+        int idle_time = (freq_max - 1) * n;
 
-        int totalInterval = 0;
-
-        while (!pq.isEmpty()) {
-            List<Character> coolDownList = new ArrayList<>();
-
-            int interval = 0;
-
-            while (interval <= n) {
-                if (!pq.isEmpty()) {
-                    char c = pq.poll();
-                    freq.put(c, freq.get(c) - 1);
-
-                    if (freq.get(c) > 0) {
-                        coolDownList.add(c);
-                    }
-                }
-                interval++;
-                totalInterval++;
-
-                if (pq.isEmpty() && coolDownList.isEmpty()) {
-                    return totalInterval;
-                }
-            }
-
-            pq.addAll(coolDownList);
+        for (int i = freq.length - 2; i >= 0 && idle_time > 0; i--) {
+            idle_time -= Math.min(freq_max - 1, freq[i]);
         }
 
-        return totalInterval;
+        idle_time = Math.max(0, idle_time);
+
+        return idle_time + tasks.length;
     }
 }

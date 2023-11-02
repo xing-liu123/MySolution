5f496dd099ef59fd16286450faedfb8aa3e33f97
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-11-02 18:30:23 -0400

    Time: 89 ms (41.78%), Space: 66.8 MB (5.16%) - LeetHub

--- 0769-largest-plus-sign/0769-largest-plus-sign.java
+++ 0769-largest-plus-sign/0769-largest-plus-sign.java
@@ -10,84 +10,32 @@ class Solution {
             grid[mine[0]][mine[1]] = 0;
         }
 
-        int max = mines.length < n * n ? 1 : 0;
-       int[][][] dp = new int[n][n][2];
+        int[][][] dp = new int[n][n][4];
 
-       if (grid[0][0] == 0) {
-           dp[0][0][0] = 0;
-           dp[0][0][1] = 0;
-       } else {
-           dp[0][0][0] = 1;
-           dp[0][0][1] = 1;
-       }
+        // left up
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dp[i][j][0] = j == 0 ? 1 : dp[i][j - 1][0] + 1;
+                    dp[i][j][1] = i == 0 ? 1 : dp[i - 1][j][1] + 1;
+                }
+            }
+        }
 
-       for (int i = 1; i < n; i++) {
-           if (grid[i][0] != 0) {
-                dp[i][0][0] = dp[i - 1][0][0] + 1;
-                dp[i][0][1] = 1;
-           } 
-       } 
+        int max = 0;
 
-       for (int i = 1; i < n; i++) {
-           if (grid[0][i] != 0) {
-                dp[0][i][1] = dp[0][i - 1][1] + 1;
-                dp[0][i][0] =  1;
-           } 
-       } 
+        // right down
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (grid[i][j] == 1) {
+                    dp[i][j][2] = j == (n - 1) ? 1 : dp[i][j + 1][2] + 1;
+                    dp[i][j][3] = i == (n - 1) ? 1 : dp[i + 1][j][3] + 1;
+                }
 
-       for (int i = 1; i < n; i++) {
-           for (int j = 1; j < n; j++) {
-               if (grid[i][j] == 0) {
-                   dp[i][j][0] = 0;
-                   dp[i][j][1] = 0;
-               } else {
-                   dp[i][j][0] = dp[i - 1][j][0] + 1;
-                   dp[i][j][1] = dp[i][j - 1][1] + 1;
-               }
-           }
-       }
-
-
-       int[][][] dp2 = new int[n][n][2];
-
-       if (grid[n - 1][n - 1] == 0) {
-           dp2[n - 1][n - 1][0] = 0;
-           dp2[n - 1][n - 1][1] = 0;
-       } else {
-           dp2[n - 1][n - 1][0] = 1;
-           dp2[n - 1][n - 1][1] = 1;
-       }
-
-       for (int i = n - 2; i >= 0; i--) {
-           if (grid[i][n - 1] != 0) {
-               dp2[i][n - 1][0] = dp2[i + 1][n - 1][0] + 1;
-                dp2[i][n - 1][1] = 1;
-           } 
-           
-       }
-
-        for (int i = n - 2; i >= 0; i--) {
-           if (grid[n - 1][i] != 0) {
-               dp2[n - 1][i][0] = 1;
-                dp2[n - 1][i][1] = dp2[n - 1][i + 1][1] + 1;
-           }
-       }
-
-
-        
-       for (int i = n - 2; i >= 0; i--) {
-           for (int j = n - 2; j >= 0; j--) {
-               if (grid[i][j] == 0) {
-                   dp2[i][j][0] = 0;
-                   dp2[i][j][1] = 0;
-               } else {
-                   dp2[i][j][0] = dp2[i + 1][j][0] + 1;
-                   dp2[i][j][1] = dp2[i][j + 1][1] + 1;
-               }
-
-               max = Math.max(max, Math.min(dp[i][j][0], Math.min(dp[i][j][1], Math.min(dp2[i][j][0], dp2[i][j][1]))));
-           }
-       }
+                int min = Math.min(Math.min(dp[i][j][0], dp[i][j][1]), Math.min(dp[i][j][2], dp[i][j][3]));
+                max = Math.max(max, min);
+            }
+        }
 
         return max;
     }

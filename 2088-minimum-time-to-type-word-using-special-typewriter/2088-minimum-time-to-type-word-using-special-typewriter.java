310c1a3b97ef337ecf1cb41990b740d57961bd4f
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-11-02 00:45:08 -0400

    Time: 0 ms (100%), Space: 39.8 MB (98.86%) - LeetHub

--- 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.java
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.java
@@ -1,10 +1,12 @@
 class Solution {
     public int minTimeToType(String word) {
         int time = 0;
+        char[] chars = word.toCharArray();
         char currLetter = 'a';
 
-        for (int i = 0; i < word.length(); i++) {
-            char nextLetter = word.charAt(i);
+        for (int i = 0; i < chars.length; i++) {
+            char nextLetter = chars[i];
+            
             int diff = nextLetter - currLetter;
             if (diff >= 0) {
                 time += Math.min(diff, -diff + 26);

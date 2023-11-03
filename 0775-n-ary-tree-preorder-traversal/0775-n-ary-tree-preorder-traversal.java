413f78ae88f964bd8513be225fc0baeb6772bb02
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2023-11-03 18:32:46 -0400

    Time: 1 ms (57.37%), Space: 44.4 MB (38.83%) - LeetHub

---
+++ 0775-n-ary-tree-preorder-traversal/0775-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> preorder(Node root) {
+        traverse(root);
+        return res;
+    }
+
+    private void traverse(Node curr) {
+        if (curr != null) {
+            res.add(curr.val);
+
+            for (Node child : curr.children) {
+                traverse(child);
+            }
+        }
+    }
+
+}

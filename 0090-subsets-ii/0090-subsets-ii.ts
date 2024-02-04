4a32c8b464d83794403869ec540ed230aa53f34d
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-02-04 17:52:10 -0500

    Time: 64 ms (81.30%), Space: 54.9 MB (5.22%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.ts
@@ -0,0 +1,22 @@
+function subsetsWithDup(nums: number[]): number[][] {
+    const res : number[][] = [];
+    const subset : number[] = [];
+    nums.sort((a, b) => a - b);
+    
+    function backtrack(start : number) : void {
+        res.push([...subset]);
+    
+        for (let i = start; i < nums.length; i++) {
+            if (i !== start && nums[i] === nums[i - 1]) {
+                continue;
+            }
+            
+            subset.push(nums[i]);
+            backtrack(i + 1);
+            subset.pop();
+        }
+    }
+
+    backtrack(0);
+    return res;
+};

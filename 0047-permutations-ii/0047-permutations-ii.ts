76c6370a3cd061f3a618fac76936496d18b85c1f
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-02-05 17:35:11 -0500

    Time: 83 ms (39.80%), Space: 54.7 MB (26.02%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.ts
@@ -0,0 +1,29 @@
+function permuteUnique(nums: number[]): number[][] {
+    const res : number[][] = [];
+    const per : number[] = [];
+    const used : boolean[] = new Array(nums.length).fill(false);
+    
+    nums.sort((a, b) => a - b);
+    
+    function backtrack() : void {
+        if (per.length === nums.length) {
+            res.push([...per]);
+            return;
+        }
+        
+        for (let i = 0; i < nums.length; i++) {
+            if (used[i] || (i !== 0 && nums[i] === nums[i - 1] && used[i - 1])) {
+                continue;
+            }
+            
+            used[i] = true;
+            per.push(nums[i]);
+            backtrack();
+            per.pop();
+            used[i] = false;
+        }
+    }
+
+    backtrack();
+    return res;
+};

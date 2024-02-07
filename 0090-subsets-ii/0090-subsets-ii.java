9c63362b9ea1c63b78ef087a16f192038a65999e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-02-07 15:05:48 -0500

    Time: 1 ms (99.90%), Space: 43.4 MB (57.84%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,19 +1,23 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
-    List<Integer> path = new ArrayList<>();;
+    List<Integer> path = new ArrayList<>();
+    
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-       Arrays.sort(nums);
+        Arrays.sort(nums);
        backtracking(nums, 0);
+        
        return res;
     }
+    
+    Set<Integer> set = new HashSet<>();
 
     private void backtracking(int[] nums, int start) {
        res.add(new ArrayList<Integer>(path));
 
        for (int i = start; i < nums.length; i++) {
-           if (i > start && nums[i] == nums[i - 1]) {
-               continue;
-           }
+            if (i != start && nums[i] == nums[i - 1]) {
+                continue;
+            }
            path.add(nums[i]);
            backtracking(nums, i + 1);
             path.remove(path.size() - 1);

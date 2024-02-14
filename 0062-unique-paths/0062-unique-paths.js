72ec2871c264fd633a8d92bc83dc3188b2bc5954
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-02-14 11:12:47 -0500

    Time: 60 ms (30.42%), Space: 49.8 MB (10.65%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    const paths = new Array(m).fill(null).map(() => new Array(n).fill(0));
+    
+    for (let i = 0; i < m; i++) {
+        paths[i][0] = 1;
+    }
+    
+    for (let j = 0; j < n; j++) {
+        paths[0][j] = 1;
+    }
+    
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            paths[i][j] = paths[i - 1][j] + paths[i][j - 1];
+        }
+    }
+    
+    return paths[m - 1][n - 1];
+};

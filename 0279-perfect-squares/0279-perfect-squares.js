d63701e293908a4d1e407acaaacf3201f6d1425b
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-02-24 11:23:19 -0500

    Time: 155 ms (60.33%), Space: 53.2 MB (37.76%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.js
+++ 0279-perfect-squares/0279-perfect-squares.js
@@ -6,10 +6,11 @@ var numSquares = function(n) {
     let dp = Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
     dp[0] = 0;
     
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j * j <= i; j++) {
-            
-            dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+    for (let i = 1; i * i <= n; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (i * i <= j) {
+                dp[j] = Math.min(dp[j], dp[j - i * i] + 1);
+            }
         }
     }
     

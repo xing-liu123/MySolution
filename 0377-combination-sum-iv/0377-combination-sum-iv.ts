9dc9df9079336f77a17d0dedeeab23cab0a9bce9
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-02-23 22:35:16 -0500

    Time: 65 ms (48.91%), Space: 52.6 MB (51.09%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.ts
@@ -0,0 +1,15 @@
+function combinationSum4(nums: number[], target: number): number {
+    let dp: number[] = Array(target + 1).fill(0);
+    
+    dp[0] = 1;
+    
+    for (let i = 1; i <= target; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i >= nums[j]) {
+                dp[i] += dp[i - nums[j]];
+            } 
+        }
+    }
+    
+    return dp[target];
+};

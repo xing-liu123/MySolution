032e0538380614b158b85a45b06bc153489d7aaa
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-01 23:57:54 -0400

    Time: 42 ms (17.13%), Space: 16.4 MB (98.80%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        i = 0
+        j = 0
+        
+        status = 1
+        
+        top = 1
+        bottom = n - 1
+        left = 0
+        right = n - 1
+        
+        for count in range(n ** 2):
+            matrix[i][j] = count + 1
+            
+            if status == 1:
+                j += 1
+                if j == right:
+                    status = 2
+                    right -=1
+            elif status == 2:
+                i += 1
+                if i == bottom:
+                    status = 3
+                    bottom -= 1
+            elif status == 3:
+                j -= 1
+                if j == left:
+                    status = 4
+                    left += 1
+            else:
+                i -= 1
+                if i == top:
+                    status = 1
+                    top += 1
+        
+        return matrix
+            
+        

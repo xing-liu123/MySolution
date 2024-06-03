bcd53a6cdff80f771a013413110ab27ebe79582f
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-03 12:59:58 -0400

    Time: 82 ms (50.00%), Space: 27 MB (93.42%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,27 +6,40 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        currA = headA
-        currB = headB
+#         currA = headA
+#         currB = headB
         
-        while currA and currB:
-            currA = currA.next
-            currB = currB.next
+#         while currA and currB:
+#             currA = currA.next
+#             currB = currB.next
             
-        newA = headA
-        newB = headB
+#         newA = headA
+#         newB = headB
         
-        while currA:
-            currA = currA.next
-            newA = newA.next
+#         while currA:
+#             currA = currA.next
+#             newA = newA.next
             
-        while currB:
-            currB = currB.next
-            newB = newB.next
+#         while currB:
+#             currB = currB.next
+#             newB = newB.next
             
-        while newA != newB:
-            newA = newA.next
-            newB = newB.next
+#         while newA != newB:
+#             newA = newA.next
+#             newB = newB.next
         
-        return newA if newA == newB else None
+#         return newA if newA == newB else None
+        if not headA or not headB:
+            return None
+
+        pointerA = headA
+        pointerB = headB
+
+        while pointerA != pointerB:
+            # If pointerA reaches the end of its list, redirect it to headB
+            pointerA = pointerA.next if pointerA else headB
+            # If pointerB reaches the end of its list, redirect it to headA
+            pointerB = pointerB.next if pointerB else headA
+
+        return pointerA  # or pointerB, they are the same at this point
         

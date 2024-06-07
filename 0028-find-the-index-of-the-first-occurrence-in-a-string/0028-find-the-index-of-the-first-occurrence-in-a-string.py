601b238ca920a825c24fecd2d8a046cb41d66bde
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-07 16:07:10 -0400

    Time: 38 ms (33.32%), Space: 16.5 MB (82.73%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,42 +1,76 @@
 class Solution:
-    def buildFailureTable(self, needle: str) -> List[str]:
-        table = [0] * len(needle)
+#     def buildFailureTable(self, needle: str) -> List[str]:
+#         table = [0] * len(needle)
         
-        i, j = 0, 1
+#         i, j = 0, 1
         
-        while j < len(needle):
-            if needle[i] == needle[j]:
-                table[j] = i + 1
-                i += 1
-                j += 1
-            elif i == 0:
-                j += 1
-            else:
-                i = table[i - 1]
+#         while j < len(needle):
+#             if needle[i] == needle[j]:
+#                 table[j] = i + 1
+#                 i += 1
+#                 j += 1
+#             elif i == 0:
+#                 j += 1
+#             else:
+#                 i = table[i - 1]
         
-        return table
+#         return table
+    
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         if len(needle) > len(haystack):
+#             return -1
+        
+#         table = self.buildFailureTable(needle)
+        
+#         i, j = 0, 0
+        
+#         while j < len(haystack):
+#             if needle[i] == haystack[j]:
+#                 if i == len(needle) - 1:
+#                     return j - i
+#                 else:
+#                     i += 1
+#                     j += 1
+#             elif i == 0:
+#                 j += 1
+#             else:
+#                 i = table[i - 1]             
+        
+#         return -1
     
     def strStr(self, haystack: str, needle: str) -> int:
         if len(needle) > len(haystack):
             return -1
         
-        table = self.buildFailureTable(needle)
+        lastOccurenceTable = self.buildLastOccurenceTable(needle)
         
-        i, j = 0, 0
+        i = 0
         
-        while j < len(haystack):
-            if needle[i] == haystack[j]:
-                if i == len(needle) - 1:
-                    return j - i
+        while i <= len(haystack) - len(needle):
+            j = len(needle) - 1
+            
+            while j >= 0 and needle[j] == haystack[i + j]:
+                j -= 1
+            
+            if j == -1:
+                return i
+            else:
+                shift = lastOccurenceTable.get(haystack[i + j], -1)
+                
+                if shift < j:
+                    i = i + j - shift
                 else:
                     i += 1
-                    j += 1
-            elif i == 0:
-                j += 1
-            else:
-                i = table[i - 1]             
         
         return -1
+        
+    def buildLastOccurenceTable(self, needle: str) -> List[str]:
+        lastOccurenceTable = {}
+        
+        for i in range(len(needle)):
+            lastOccurenceTable[needle[i]] = i
+        
+        return lastOccurenceTable
         
         
         

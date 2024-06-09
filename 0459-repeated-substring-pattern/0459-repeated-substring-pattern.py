e5a37570a85426ab162267241595b8efea7dc1f2
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-09 00:39:51 -0400

    Time: 36 ms (89.25%), Space: 16.6 MB (65.00%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,18 +1,22 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        table = [0] * len(s)
+#         table = [0] * len(s)
         
-        i, j = 0, 1
+#         i, j = 0, 1
         
-        while j < len(s):
-            if s[i] == s[j]:
-                table[j] = i + 1
-                i += 1
-                j += 1
-            elif i == 0:
-                j += 1
-            else:
-                i = table[i - 1]
+#         while j < len(s):
+#             if s[i] == s[j]:
+#                 table[j] = i + 1
+#                 i += 1
+#                 j += 1
+#             elif i == 0:
+#                 j += 1
+#             else:
+#                 i = table[i - 1]
         
-        return len(s) % (len(s) - table[-1]) == 0 and table[-1] != 0
+#         return len(s) % (len(s) - table[-1]) == 0 and table[-1] != 0
+
+        ss = s + s
+    
+        return s in ss[1:-1]
         

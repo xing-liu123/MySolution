74cf4260db10ff00a073fa431105011888fe1544
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-13 13:35:12 -0400

    Time: 91 ms (44.30%), Space: 21.2 MB (52.32%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        map = {}
+        
+        for num in nums:
+            if num in map:
+                map[num] += 1
+            else:
+                map[num] = 1
+        
+        
+        
+        return [item[0] for item in sorted(map.items(), key=lambda item: item[1], reverse=True)[:k]] 
+        

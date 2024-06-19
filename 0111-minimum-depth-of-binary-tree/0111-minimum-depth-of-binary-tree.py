c49bc7e1209b843faf9330eee4bf516d3cc40df2
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-19 19:43:59 -0400

    Time: 212 ms (90.59%), Space: 43.2 MB (70.78%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,19 +7,41 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+        
+#         return self.depth(root, 1)
+    
+#     def depth(self, node: TreeNode, depth: int) -> int:
+#         if not node.left and not node.right:
+#             return depth
+        
+#         left = self.depth(node.left, depth + 1) if node.left else sys.maxsize
+#         right = self.depth(node.right, depth + 1) if node.right else sys.maxsize
+        
+#         return min(left, right)
         if not root:
             return 0
         
-        return self.depth(root, 1)
-    
-    def depth(self, node: TreeNode, depth: int) -> int:
-        if not node.left and not node.right:
-            return depth
+        queue = deque([root])
         
-        left = self.depth(node.left, depth + 1) if node.left else sys.maxsize
-        right = self.depth(node.right, depth + 1) if node.right else sys.maxsize
+        depth = 0
         
-        return min(left, right)
+        while queue:
+            depth += 1
+            size = len(queue)
+            
+            for _ in range(size):
+                curr = queue.popleft()
+                
+                if not curr.left and not curr.right:
+                    return depth
+                
+                if curr.left: queue.append(curr.left)
+                if curr.right: queue.append(curr.right)
+        
+        return 1
+        
         
         
         

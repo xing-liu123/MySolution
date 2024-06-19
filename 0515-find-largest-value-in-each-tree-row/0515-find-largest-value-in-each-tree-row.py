0f265289cd161b1986a950622b887e49222fd642
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-19 15:24:56 -0400

    Time: 42 ms (63.40%), Space: 18.1 MB (92.21%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -7,30 +7,46 @@ from collections import deque
 #         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+#         res = []
+        
+#         if not root:
+#             return res
+        
+#         queue = deque([root])
+        
+#         while queue:
+            
+#             largest = -sys.maxsize - 1
+#             size = len(queue)
+            
+#             for _ in range(size):
+#                 node = queue.popleft()
+                
+#                 if node.left:
+#                     queue.append(node.left)
+                
+#                 if node.right:
+#                     queue.append(node.right)
+                
+#                 if node.val > largest:
+#                     largest = node.val
+            
+#             res.append(largest)
+        
+#         return res
         res = []
-        
-        if not root:
-            return res
-        
-        queue = deque([root])
-        
-        while queue:
-            
-            largest = -sys.maxsize - 1
-            size = len(queue)
-            
-            for _ in range(size):
-                node = queue.popleft()
-                
-                if node.left:
-                    queue.append(node.left)
-                
-                if node.right:
-                    queue.append(node.right)
-                
-                if node.val > largest:
-                    largest = node.val
-            
-            res.append(largest)
-        
+        self.order(res, root, 0)
         return res
+        
+    def order(self, res: List[int], curr: TreeNode, depth: int):
+        if not curr:
+            return
+        
+        if len(res) < depth + 1:
+            res.append(curr.val)
+        else:
+            res[depth] = max(res[depth], curr.val)
+        
+        self.order(res, curr.left, depth + 1)
+        self.order(res, curr.right, depth + 1)
+        

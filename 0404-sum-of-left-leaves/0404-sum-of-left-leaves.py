0f9abf3efc37f5998e71a2f9594d9aa4b1799092
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-20 14:57:51 -0400

    Time: 39 ms (35.91%), Space: 16.7 MB (36.06%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -6,9 +6,9 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        return self.sumOfLeft(root, 0, False)
+        return self.sumOfLeft(root, False)
     
-    def sumOfLeft(self, curr, currSum, isLeft) -> int:
+    def sumOfLeft(self, curr, isLeft) -> int:
         if not curr:
             return 0
         
@@ -18,8 +18,8 @@ class Solution:
             else:
                 return 0
         
-        left = self.sumOfLeft(curr.left, currSum, True) if curr.left else 0
+        left = self.sumOfLeft(curr.left, True) if curr.left else 0
             
-        right = self.sumOfLeft(curr.right, currSum, False) if curr.right else 0
+        right = self.sumOfLeft(curr.right, False) if curr.right else 0
         
         return left + right

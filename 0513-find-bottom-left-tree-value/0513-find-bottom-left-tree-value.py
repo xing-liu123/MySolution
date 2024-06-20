acea3eb7f0d349fe2111ac68cfb4eb9349a7a650
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-20 15:31:45 -0400

    Time: 47 ms (49.29%), Space: 19.8 MB (10.61%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -7,21 +7,39 @@ from collections import deque
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        res = root.val
+#         res = root.val
         
-        queue = deque([root])
+#         queue = deque([root])
         
-        while queue:
-            size = len(queue)
-            res = queue[0].val
+#         while queue:
+#             size = len(queue)
+#             res = queue[0].val
             
-            for _ in range(size):
-                curr = queue.popleft()
+#             for _ in range(size):
+#                 curr = queue.popleft()
                 
-                if curr.left:
-                    queue.append(curr.left)
+#                 if curr.left:
+#                     queue.append(curr.left)
                     
-                if curr.right:
-                    queue.append(curr.right)
+#                 if curr.right:
+#                     queue.append(curr.right)
         
-        return res
+#         return res
+        return self.findLeft(root, 0)[1]
+
+    def findLeft(self, curr, depth) -> (int, int):
+        if not curr.left and not curr.right:
+            return depth, curr.val
+        
+                
+        leftDepth, leftVal = self.findLeft(curr.left, depth + 1) if curr.left else (-1, 0)
+        
+        rightDepth, rightVal = self.findLeft(curr.right, depth + 1) if curr.right else (-1, 0)
+
+                
+        if leftDepth >= rightDepth:
+            return leftDepth, leftVal
+        else:
+            return rightDepth, rightVal
+        
+            

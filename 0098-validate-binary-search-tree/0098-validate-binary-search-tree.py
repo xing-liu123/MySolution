3fbed5b3f8d61d7ba7de8a88f06c4111a8ed1f19
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-21 09:49:23 -0400

    Time: 34 ms (92.20%), Space: 18.3 MB (69.25%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,22 +6,41 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        arr = []
-        def traverse(curr) -> bool:
-            if not curr:
-                return True
+#         arr = []
+#         def traverse(curr) -> bool:
+#             if not curr:
+#                 return True
             
-            if not traverse(curr.left):
-                return False
+#             if not traverse(curr.left):
+#                 return False
             
-            if len(arr) > 0 and curr.val <= arr[-1]:
-                return False
+#             if len(arr) > 0 and curr.val <= arr[-1]:
+#                 return False
             
-            arr.append(curr.val)
+#             arr.append(curr.val)
             
-            if not traverse(curr.right):
-                return False
-            else:
-                return True
+#             if not traverse(curr.right):
+#                 return False
+#             else:
+#                 return True
         
-        return traverse(root)
+#         return traverse(root)
+        prev = None
+        curr = root
+        stack = []
+        
+        while curr or stack:
+            if curr:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                
+                if prev and prev.val >= curr.val:
+                    return False
+                
+                prev = curr
+                curr = curr.right
+        
+        return True
+                    

b7f30bafe2b56dd6832c2350a7370666754cb958
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-22 16:14:46 -0400

    Time: 61 ms (46.09%), Space: 18.4 MB (76.96%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.py
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.py
@@ -6,19 +6,35 @@
 #         self.right = right
 class Solution:
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        currSum = 0
+#         currSum = 0
         
-        def convert(curr):
-            nonlocal currSum
-            if not curr: 
-                return
+#         def convert(curr):
+#             nonlocal currSum
+#             if not curr: 
+#                 return
             
-            convert(curr.right)
-            currSum += curr.val
-            curr.val = currSum
-            convert(curr.left)
+#             convert(curr.right)
+#             currSum += curr.val
+#             curr.val = currSum
+#             convert(curr.left)
             
             
-            return curr
+#             return curr
             
-        return convert(root)
+#         return convert(root)
+        curr = root
+        currSum = 0
+        stack = []
+        
+        while stack or curr:
+            if curr:
+                stack.append(curr)
+                curr = curr.right
+            else:
+                curr = stack.pop()
+                currSum += curr.val
+                curr.val = currSum
+                
+                curr = curr.left
+        
+        return root

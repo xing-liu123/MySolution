2e46ff59f7624f0622e793daa0b24057603165c3
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-06-26 17:04:56 -0400

    Time: 38 ms (65.59%), Space: 16.6 MB (95.86%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,20 +1,28 @@
 class Solution:
-    def __init__(self):
-        self.path = []
-        self.res = []
+    
 
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        path = []
+        res = []
+        
         nums.sort()
-        self.backtracking(nums, 0)
-        return self.res
+        
+        def backtracking(start):
+            res.append(path.copy())
+            
+            if start == len(nums):
+                return
+            
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                    
+                path.append(nums[i])
+                backtracking(i + 1)
+                path.pop()
+                
+        backtracking(0)
+        
+        return res
 
-    def backtracking(self, nums: List[int], start: int):
-        
-        self.res.append(copy.copy(self.path))
-        
-        for i in range(start, len(nums)):
-            if i > start and nums[i] == nums[i - 1]:
-                continue
-            self.path.append(nums[i])
-            self.backtracking(nums, i + 1)
-            self.path.pop()
+

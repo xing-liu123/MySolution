56a7b7625ee76dbbc6bbe98ba8f1894530878fc8
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-07-18 00:27:28 -0400

    Time: 993 ms (39.49%), Space: 38 MB (33.61%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,14 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 1:
+            return 0
+
         dp = [[0] * 4 for _ in range(len(prices))]
 
         dp[0][0] = -prices[0]
         dp[0][2] = -prices[0]
 
         for i in range(1, len(prices)):
-            dp[i][0] = max(-prices[i], dp[i - 1][0])
+            dp[i][0] = max(dp[i - 1][0], - prices[i])
             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
-            dp[i][2] = max(dp[i - 1][1] - prices[i], dp[i - 1][2])
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] - prices[i])
             dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] + prices[i])
-            
-        return max(dp[len(prices) - 1][0], dp[len(prices) - 1][1], dp[len(prices) - 1][2], dp[len(prices) - 1][3])
+        
+        return dp[-1][3]

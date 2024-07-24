070943174d2c3e94d812773b7e330f990e9beea3
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-07-24 16:43:22 -0400

    Time: 505 ms (89.48%), Space: 24.1 MB (75.48%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,30 +1,23 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         res = []
-        visited = dict()
+        safe = dict()
 
-        def dfs(curr):
-            if not graph[curr]:
-                visited[curr] = True
-                return
-                
-            visited[curr] = False
-            count = 0
+        def dfs(curr) -> bool:
+            if curr in safe:
+                return safe[curr]
+
+            safe[curr] = False
 
             for neighbor in graph[curr]:
-                if not neighbor in visited:
-                    dfs(neighbor)
+                if not dfs(neighbor):
+                    return False
                 
-                if visited[neighbor]:
-                    count += 1
-            
-            visited[curr] = count == len(graph[curr])
-
-        for i in range(len(graph)):
-            if not i in visited:
-                dfs(i)
-            
-            if visited[i]:
+            safe[curr] = True
+            return True
+        
+        for i in range(len(graph)):  
+            if dfs(i):
                 res.append(i)
-
+                            
         return res

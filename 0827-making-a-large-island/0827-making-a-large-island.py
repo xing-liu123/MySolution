0f9b2aefe0d9d16ad0183d73367ef9d9ad7a244f
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-07-23 21:47:59 -0400

    Time: 1966 ms (70.31%), Space: 24.3 MB (71.79%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,33 +1,52 @@
 class Solution:
+    def __init__(self):
+        self.areas = defaultdict(int)
+        self.grid = []
+        self.dr = [1, -1, 0, 0]
+        self.dc = [0, 0, 1, -1]
+        self.m = 0
+        self.n = 0
+        self.visited = []
+
     def largestIsland(self, grid: List[List[int]]) -> int:
+        self.grid = grid
+        self.m, self.n = len(grid), len(grid[0])
+        self.visited = [[False] * self.n for _ in range(self.m)]
+        marker = 2
+          
+        for i in range(self.m):
+            for j in range(self.n):
+                if self.grid[i][j] == 1:
+                    self.dfs(i, j, marker)
+                    marker += 1
+
         largest = 0
-        area = 0
-        m, n = len(grid), len(grid[0])
-        dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
 
-        def dfs(row, col, visited):
-            nonlocal area
-            nonlocal largest
-            area += 1
-            visited[row][col] = True
+        for i in range(self.m):
+            for j in range(self.n):
+                if self.grid[i][j] == 0:
+                    visited = set()
+                    area = 1
+                    for k in range(4):
+                        x, y = i + self.dr[k], j + self.dc[k]
 
-            largest = max(largest, area)
+                        if 0 <= x < self.m and 0 <= y < self.n and self.grid[x][y] > 1:
+                            if grid[x][y] not in visited:
+                                area += self.areas[grid[x][y]]
+                                visited.add(grid[x][y])
+                                
+                    largest = max(largest, area)
+                
+        return largest if largest != 0 else self.m * self.n
 
+    def dfs(self, row, col, marker):
+            self.grid[row][col] = marker
+            self.areas[marker] += 1
             for k in range(4):
-                x, y = row + dr[k], col + dc[k]
+                x, y = row + self.dr[k], col + self.dc[k]
 
-                if 0 <= x < m and 0 <= y < n and not visited[x][y] and grid[x][y] == 1:
-                    dfs(x, y, visited)
-
-    
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 0:
-                    area = 0
-                    visited = [[False] * n for _ in range(m)]
-                    dfs(i, j, visited)
-
-        return largest if area != 0 else m * n
+                if 0 <= x < self.m and 0 <= y < self.n and self.grid[x][y] == 1:
+                    self.dfs(x, y, marker)
 
 
 

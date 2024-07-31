6c77d00748362d3c6b2ee5211e476f655058a555
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-07-30 23:51:06 -0400

    Time: 867 ms (6.73%), Space: 20.1 MB (5.5%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -2,27 +2,24 @@ from collections import deque
 class Solution:
     def minDays(self, n: int) -> int:
         
-        visited = set()
+        visited = dict()
         queue = deque([(n, 0)])
 
         while queue:
             curr, days = queue.popleft()
 
-            if curr in visited:
-                continue
-
             if curr == 0:
                 return days
 
-            visited.add(curr)
+            visited[curr] = days
 
-            if curr % 3 == 0 and curr // 3 not in visited:
+            if curr % 3 == 0 and (curr // 3 not in visited or days + 1 < visited[curr // 3]):
                 queue.append((curr // 3, days + 1))
 
-            if curr % 2 == 0 and curr // 2 not in visited:
+            if curr % 2 == 0 and (curr // 2 not in visited or days + 1 < visited[curr // 2]):
                 queue.append((curr // 2, days + 1))
             
-            if curr - 1 not in visited:
+            if curr - 1 not in visited or days + 1 < visited[curr - 1]:
                 queue.append((curr - 1, days + 1))
 
         return -1

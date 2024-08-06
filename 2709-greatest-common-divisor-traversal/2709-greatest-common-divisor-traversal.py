becd376edd021b962d8b9f3b9e757fd02503f6dd
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-08-05 20:36:43 -0400

    Time: 3595 ms (31.9%), Space: 36.5 MB (25.47%) - LeetHub

--- 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.py
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.py
@@ -1,52 +1,50 @@
+from collections import defaultdict
+import math
+
 class Solution:
     def canTraverseAllPairs(self, nums: List[int]) -> bool:
         n = len(nums)
-
         if n == 1:
             return True
-            
-        root = dict()
-        size = dict()
 
-        num_map = defaultdict(lambda: 0)
+        root = list(range(n))
+        size = [1] * n
 
-        for num in nums:
-            num_map[num] += 1
+        def find(x):
+            if root[x] != x:
+                root[x] = find(root[x])
+            return root[x]
+
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                root[rootY] = rootX
+                size[rootX] += size[rootY]
+
+        factor_map = defaultdict(list)
         
+        for i, num in enumerate(nums):
+            if num > 1:
+                # Finding all prime factors
+                for j in range(2, int(math.sqrt(num)) + 1):
+                    if num % j == 0:
+                        factor_map[j].append(i)
+                        while num % j == 0:
+                            num //= j
+                if num > 1:  # num itself is a prime
+                    factor_map[num].append(i)
 
-        def find(u):
-            if root[u] != u:
-                root[u] = find(root[u])
-            return root[u]
+        # Step 2: Union numbers with shared factors
+        for indices in factor_map.values():
+            first = indices[0]
+            for index in indices[1:]:
+                union(first, index)
 
-        def union(u, v):
-            root_u = find(u)
-            root_v = find(v)
+        # Step 3: Check if all numbers are connected
+        return size[find(0)] == n
 
-            if root_u != root_v:
-                root[root_v] = root_u
-                size[root_u] += size[root_v]
-        
-        def gcd(u, v) -> bool:
-            while v != 0:
-                u, v = v, u % v
-            
-            return u
-
-        if len(num_map) == 1:
-            if nums[0] != 1:
-                return True
-            else:
-                return False
-
-        
-        for num in num_map.keys():
-            root[num] = num
-            size[num] = num_map[num]
-        
-        for num1 in num_map.keys():
-            for num2 in num_map.keys():
-                if num1 != num2 and gcd(num1, num2) > 1:
-                    union(num1, num2)
-        
-        return max(size.values()) == n
+# Example usage:
+sol = Solution()
+nums = [4, 6, 15, 35]
+print(sol.canTraverseAllPairs(nums))  # True or False

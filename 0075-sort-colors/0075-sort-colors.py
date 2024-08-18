29fc52dff6d174fc29a7ab2b31d98e0820bebf0a
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-08-18 00:03:29 -0400

    Time: 38 ms (51.52%), Space: 16.5 MB (21.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -9,19 +9,17 @@ class Solution:
         def partition(left, right):
             pivot = nums[right]
             i = left - 1
-
             for j in range(left, right):
                 if nums[j] < pivot:
                     i += 1
                     switch(i, j)
             switch(i + 1, right)
-
             return i + 1
 
-        def quicksort(left, right):
+        def quickSort(left, right):
             if left < right:
-                mid = partition(left, right)
-                quicksort(left, mid - 1)
-                quicksort(mid + 1, right)
-        
-        quicksort(0, len(nums) - 1)
+                pi = partition(left, right)
+                quickSort(left, pi - 1)
+                quickSort(pi + 1, right)
+
+        quickSort(0, len(nums) - 1)

2abace95658b47fd3d6301271e877b091672fd51
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-08-20 12:13:59 -0400

    Time: 236 ms (28.2%), Space: 18.9 MB (99.8%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,13 +3,18 @@ class Solution:
         for i in range(1, len(nums)):
             nums[i] += nums[i - 1]
 
-        sum_map = defaultdict(lambda: 0)
+        sum_map = {}
         sum_map[0] = 1
 
         count = 0
 
         for s in nums:
-            count += sum_map[s - k]
-            sum_map[s] += 1            
+            if s - k in sum_map:
+                count += sum_map[s - k]
+            
+            if s in sum_map:
+                sum_map[s] += 1
+            else:
+                sum_map[s] = 1
 
         return count

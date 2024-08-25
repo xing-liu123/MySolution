909eaeae2023496be62820d512b639f667aaa308
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-08-25 08:58:22 -0400

    Time: 321 ms (37.66%), Space: 49.6 MB (79.75%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -2,7 +2,6 @@ class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
         size = 2 ** k
 
-        
         if len(s) - k + 1 < size:
             return False
             
@@ -22,6 +21,5 @@ class Solution:
             if len(s) - i - 1 < size - len(code_set):
                 return False
 
-        if len(code_set) == size:
-                return True 
+        return len(code_set) == size
         

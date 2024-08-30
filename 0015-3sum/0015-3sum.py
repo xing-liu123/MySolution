8117c3fc8c0b904375b927971527631ccb0dd732
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-08-29 22:43:03 -0400

    Time: 678 ms (67.91%), Space: 20.7 MB (71.47%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums = sorted(nums)
+        n = len(nums)
+        nums.sort()
         
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                break
-            
+        if nums[0] > 0:
+            return []
+
+        res = []
+
+        for i in range(n):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            nums_map = set()
-            
-            for j in range(i + 1, len(nums)):
-                if j > i + 2 and nums[j] == nums[j - 1] == nums[j - 2]:
-                    continue
                 
-                num = -nums[i] - nums[j]
-                
-                if num in nums_map:
-                    res.append([nums[i], num, nums[j]])
-                    nums_map.remove(num)
+            left, right = i + 1, n - 1
+
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+
+                if sum > 0:
+                    right -= 1
+                elif sum < 0:
+                    left += 1
                 else:
-                    nums_map.add(nums[j])
-                
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    while left < right and nums[right - 1] == nums[right]:
+                        right -= 1
+                    
+                    left += 1
+                    right -= 1
+
         return res
+

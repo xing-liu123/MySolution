fc71bc955650009b261dd9fd1ed24aea47485272
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-09-05 18:50:24 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,28 +1,31 @@
 class Solution:
-    @staticmethod
-    def match(l1: list, l2: list) -> bool:
-        for i in range(len(l1)):
-            if l1[i] != l2[i]:
-                return False
-            
-        return True
+   
 
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s2) < len(s1):
             return False
 
-        s1list = [0] * 26
-        s2list = [0] * 26
+        s1_map = defaultdict(int)
+        s2_map = defaultdict(int)
 
         for i in range(len(s1)):
-            s1list[ord(s1[i]) - ord('a')] += 1
-            s2list[ord(s2[i]) - ord('a')] += 1
+            s1_map[s1[i]] += 1
+            s2_map[s2[i]] += 1
         
+        def isMatch():
+            for k, v in s2_map.items():
+                if s1_map[k] != v:
+                    return False
+            
+            return True
+
         for i in range(len(s2) - len(s1)):
-            if self.match(s1list, s2list):
-                return True;
-            else:
-                s2list[ord(s2[i]) - ord('a')] -= 1
-                s2list[ord(s2[i + len(s1)]) - ord('a')] += 1
+            if isMatch():
+                return True
+            
+            s2_map[s2[i]] -= 1
+            s2_map[s2[i + len(s1)]] += 1
+
+        return False
+
         
-        return self.match(s1list, s2list)

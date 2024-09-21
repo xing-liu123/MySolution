62e643c8adee8d32a6ce3a9c041462600620e143
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-09-21 09:40:56 -0400

    Time: 204 ms (6.11%), Space: 28.1 MB (12.06%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -2,28 +2,19 @@ import numpy as np
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        grid = np.array(grid)
-
-        oneInCol = np.sum(grid, axis = 0)
 
         for row in range(m):
             if grid[row][0] == 0:
-                oneInCol[0] += 1
-                for col in range(1, n):
-                    if grid[row][col] == 0:
-                        oneInCol[col] += 1
-                    else:
-                        oneInCol[col] -= 1
-
-        oneInCol[0] = m
-
-        exp = n - 1
+                
+                for col in range(n):
+                    grid[row][col] ^= 1
 
         res = 0
 
-        for base in oneInCol:
-            res += max(base, m - base) * 2 ** (exp)
-            exp -= 1
+        for col in range(n):
+            ones = sum(grid[row][col] for row in range(m))
+            res += max(ones, m - ones) * (1 << (n - col - 1))
+
         
         return res
 

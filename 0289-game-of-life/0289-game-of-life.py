7e01eb070a3794f4fa0d24a77440411cb5ad9c93
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-08 00:35:37 -0500

    Time: 0 ms (100%), Space: 16.6 MB (79.33%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -4,20 +4,31 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         
-        dr = [1, 1, 1, 0, 0, -1, -1, -1]
-        dc = [1, 0, -1, 1, -1, 1, 0, -1]
-        boardCopy = copy.deepcopy(board)
+        dr = [1, 1, 1, 0]
+        dc = [1, 0, -1, 1]
         m, n = len(board), len(board[0])
 
+        lastRowCopy = None
+        leftValue = 0
+
         for i in range(m):
+            rowCopy = copy.copy(board[i])
             for j in range(n):
                 surroundingSum = 0
-                for k in range(8):
+                for k in range(4):
                     x = i + dr[k]
                     y = j + dc[k]
 
                     if 0 <= x < m and 0 <= y < n:
-                        surroundingSum += boardCopy[x][y]
+                        surroundingSum += board[x][y]
+
+                if lastRowCopy:
+                    for col in range(j - 1, j + 2):
+                        if 0 <= col < n:
+                             surroundingSum += lastRowCopy[col]
+                if j != 0:
+                    surroundingSum += leftValue
+                leftValue = board[i][j]
 
                 if board[i][j] == 0:
                     if surroundingSum == 3:
@@ -26,4 +37,4 @@ class Solution:
                     if surroundingSum < 2 or surroundingSum > 3:
                         board[i][j] = 0
 
-        
+            lastRowCopy = copy.copy(rowCopy)

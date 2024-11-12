8fc072187d8ef189201d9375a505678f9212214c
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-12 12:59:42 -0500

    Time: 0 ms (100%), Space: 16.6 MB (43.68%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -2,24 +2,44 @@ class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         
         matrix = [[0] * n for _ in range(n)]
-        
-        # Directions: right, down, left, up
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        
-        cur_dir = 0  # Start with moving right
-        row, col = 0, 0  # Start at the top-left corner
-        
+        currRow = 0
+        currCol = 0
+        currDirection = 0 # 0 for right, 1 for down, 2 for left, 3 for up
+        top, bottom, left, right = 1, n - 1, 0, n - 1
+
         for num in range(1, n * n + 1):
-            matrix[row][col] = num
-            
-            next_row, next_col = row + directions[cur_dir][0], col + directions[cur_dir][1]
-            
-            if 0 <= next_row < n and 0 <= next_col < n and matrix[next_row][next_col] == 0:
-                row, col = next_row, next_col
-            else:
-                cur_dir = (cur_dir + 1) % 4
-                row, col = row + directions[cur_dir][0], col + directions[cur_dir][1]
+            matrix[currRow][currCol] = num
+
+            if currDirection == 0:
+                if currCol == right:
+                    currRow += 1
+                    currDirection = 1
+                    right -= 1
+                else:
+                    currCol += 1
+            elif currDirection == 1:
+                if currRow == bottom:
+                    currCol -= 1
+                    currDirection = 2
+                    bottom -= 1
+                else:
+                    currRow += 1
+            elif currDirection == 2:
+                if currCol == left:
+                    currRow -= 1
+                    currDirection = 3
+                    left += 1
+                else:
+                    currCol -= 1
+            elif currDirection == 3:
+                if currRow == top:
+                    currCol += 1
+                    currDirection = 0
+                    top += 1
+                else:
+                    currRow -= 1
         
         return matrix
+
+
             
-        

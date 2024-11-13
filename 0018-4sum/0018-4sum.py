6e6b3a8341cc83981a136b17d46e06befe98d110
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-13 01:02:08 -0500

    Time: 373 ms (70.45%), Space: 16.7 MB (65.62%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,12 +1,17 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
-        nums.sort()
         res = []
+        n = len(nums)
+
+        if n < 4:
+            return res
+
+        nums.sort()
 
         for i in range(n):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+
             for j in range(i + 1, n):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
@@ -14,26 +19,25 @@ class Solution:
                 left, right = j + 1, n - 1
 
                 while left < right:
-                    sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    currSum = nums[i] + nums[j] + nums[left] + nums[right]
 
-                    if sum > target:
-                        right -= 1
-                    elif sum < target:
+                    if currSum < target:
                         left += 1
+                    elif currSum > target:
+                        right -= 1
                     else:
                         res.append([nums[i], nums[j], nums[left], nums[right]])
 
-                        while left < right and nums[left] == nums[left + 1]:
+                        while left < right and nums[left + 1] == nums[left]:
                             left += 1
 
-                        while left < right and nums[right] == nums[right - 1]:
+                        while left < right and nums[right - 1] == nums[right]:
                             right -= 1
 
                         left += 1
                         right -= 1
 
         return res
-                    
 
 
         # res = []

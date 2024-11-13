d0fa38383ccf00b99159df39046b16a80c4367ba
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-12 20:03:18 -0500

    Time: 1020 ms (8.39%), Space: 37.3 MB (5.21%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -74,26 +74,53 @@ class Solution:
         # mergeSort(0, n - 1)
 
         # return nums
-        def quicksort(left, right):
-            if left < right:
-                pivotVal = nums[(left + right) // 2]
-                i, j = left,right
+        temp_arr = [0] * len(nums)
+        
+        # Function to merge two sub-arrays in sorted order.
+        def merge(left: int, mid: int, right: int):
+            # Calculate the start and sizes of two halves.
+            start1 = left
+            start2 = mid + 1
+            n1 = mid - left + 1
+            n2 = right - mid
 
-                while i <= j:
-                    while nums[i] < pivotVal:
-                        i += 1
+            # Copy elements of both halves into a temporary array.
+            for i in range(n1):
+                temp_arr[start1 + i] = nums[start1 + i]
+            for i in range(n2):
+                temp_arr[start2 + i] = nums[start2 + i]
 
-                    while nums[j] > pivotVal:
-                        j -= 1
+            # Merge the sub-arrays 'in tempArray' back into the original array 'arr' in sorted order.
+            i, j, k = 0, 0, left
+            while i < n1 and j < n2:
+                if temp_arr[start1 + i] <= temp_arr[start2 + j]:
+                    nums[k] = temp_arr[start1 + i]
+                    i += 1
+                else:
+                    nums[k] = temp_arr[start2 + j]
+                    j += 1
+                k += 1
 
-                    if i <= j:
-                        nums[i], nums[j] = nums[j], nums[i]
-                        i += 1
-                        j -= 1
+            # Copy remaining elements
+            while i < n1:
+                nums[k] = temp_arr[start1 + i]
+                i += 1
+                k += 1
+            while j < n2:
+                nums[k] = temp_arr[start2 + j]
+                j += 1
+                k += 1
 
-                quicksort(left, j)
-                quicksort(i, right) 
-
-        quicksort(0, n - 1)
-
+        # Recursive function to sort an array using merge sort
+        def merge_sort(left: int, right: int):
+            if left >= right:
+                return
+            mid = (left + right) // 2
+            # Sort first and second halves recursively.
+            merge_sort(left, mid)
+            merge_sort(mid + 1, right)
+            # Merge the sorted halves.
+            merge(left, mid, right)
+    
+        merge_sort(0, len(nums) - 1)
         return nums

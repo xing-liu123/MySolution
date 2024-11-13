3274420ff92a92dd2f3bf473e7811c48f58ea54d
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-13 00:33:01 -0500

    Time: 2 ms (56.89%), Space: 17.9 MB (12.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        res = []
-        nums_map = {}
-        
-        for index, num in enumerate(nums):
-            if target - num in nums_map:
-                res.append(index)
-                res.append(nums_map[target - num])
-                return res
-            
-            nums_map[num] = index
-        
-        return res
+        indexMap = {}
+
+        for idx, num in enumerate(nums):
+            if target - num in indexMap:
+                return [idx, indexMap[target - num]]
+
+            indexMap[num] = idx
+
+        return None

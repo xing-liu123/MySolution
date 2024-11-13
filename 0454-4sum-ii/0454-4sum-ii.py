964f7014add9e95fc7df85089b25bc243b65faa8
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-13 00:38:35 -0500

    Time: 284 ms (88.87%), Space: 17.1 MB (11.36%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,16 +1,16 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        sums = defaultdict(int)
-        
+        sumMap = defaultdict(int)
+
         for num1 in nums1:
             for num2 in nums2:
-                sums[num1 + num2] += 1
-        
+                sumMap[num1 + num2] += 1
+
         count = 0
-        
+
         for num3 in nums3:
             for num4 in nums4:
-                count += sums[-num3 - num4]
-        
+                count += sumMap[-num3 - num4]
+
         return count
         

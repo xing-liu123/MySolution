45f3d1814cf4bf0e1639e848702fb67cba3c4c1a
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-12 19:57:41 -0500

    Time: 9759 ms (5.02%), Space: 33.7 MB (13.36%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -24,53 +24,76 @@ class Solution:
         #     nums[i], nums[currMin] = nums[currMin], nums[i]
         
         # return nums
-        arr = [0] * n
+        # arr = [0] * n
 
-        def merge(left, mid, right):
-            leftIndex = left
-            rightIndex = mid + 1
-            index = 0
+        # def merge(left, mid, right):
+        #     leftIndex = left
+        #     rightIndex = mid + 1
+        #     index = 0
 
-            for i in range(left, right + 1):
-                arr[i] = nums[i]
+        #     for i in range(left, right + 1):
+        #         arr[i] = nums[i]
 
-            while leftIndex <= mid and rightIndex <= right:
+        #     while leftIndex <= mid and rightIndex <= right:
         
-                while leftIndex <= mid and arr[leftIndex] <= arr[rightIndex]:
-                    nums[left + index] = arr[leftIndex]
-                    leftIndex += 1
-                    index += 1
+        #         while leftIndex <= mid and arr[leftIndex] <= arr[rightIndex]:
+        #             nums[left + index] = arr[leftIndex]
+        #             leftIndex += 1
+        #             index += 1
 
-                while rightIndex <= right and arr[rightIndex] < arr[leftIndex]:
-                    nums[left + index] = arr[rightIndex]
-                    rightIndex += 1
-                    index += 1
+        #         while rightIndex <= right and arr[rightIndex] < arr[leftIndex]:
+        #             nums[left + index] = arr[rightIndex]
+        #             rightIndex += 1
+        #             index += 1
 
-            while leftIndex <= mid:
-                nums[left + index] = arr[leftIndex]
-                leftIndex += 1
-                index += 1
+        #     while leftIndex <= mid:
+        #         nums[left + index] = arr[leftIndex]
+        #         leftIndex += 1
+        #         index += 1
 
-            while rightIndex <= right:
-                nums[left + index] = arr[rightIndex]
-                rightIndex += 1
-                index += 1
+        #     while rightIndex <= right:
+        #         nums[left + index] = arr[rightIndex]
+        #         rightIndex += 1
+        #         index += 1
             
-        def mergeSort(left, right):
-            if right - left < 10:
-                for i in range(left + 1, right + 1):
-                    j = i
-                    while j > 0 and nums[j] > nums[j - 1]:
-                        nums[j], nums[j - 1] = nums[j - 1], nums[j]
+        # def mergeSort(left, right):
+        #     if right - left < 10:
+        #         for i in range(left + 1, right + 1):
+        #             j = i
+        #             while j > 0 and nums[j] > nums[j - 1]:
+        #                 nums[j], nums[j - 1] = nums[j - 1], nums[j]
 
 
-            if left < right:
+        #     if left < right:
                 
-                mid = (left + right) // 2
-                mergeSort(left, mid)
-                mergeSort(mid + 1, right)
-                merge(left, mid, right)
+        #         mid = (left + right) // 2
+        #         mergeSort(left, mid)
+        #         mergeSort(mid + 1, right)
+        #         merge(left, mid, right)
 
-        mergeSort(0, n - 1)
+        # mergeSort(0, n - 1)
+
+        # return nums
+        def quicksort(left, right):
+            if left < right:
+                pivotVal = nums[(left + right) // 2]
+                i, j = left,right
+
+                while i <= j:
+                    while nums[i] < pivotVal:
+                        i += 1
+
+                    while nums[j] > pivotVal:
+                        j -= 1
+
+                    if i <= j:
+                        nums[i], nums[j] = nums[j], nums[i]
+                        i += 1
+                        j -= 1
+
+                quicksort(left, j)
+                quicksort(i, right) 
+
+        quicksort(0, n - 1)
 
         return nums

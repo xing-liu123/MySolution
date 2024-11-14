a838967d5f40bc0445521ca8870c350c9d9f9297
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-14 15:06:45 -0500

    Time: 0 ms (100%), Space: 16.6 MB (37.59%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,22 +6,23 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        res = []
         if not root:
-            return res
+            return ""
         
-        self.traverse(root, res, "")
-        
+        res = []
+
+        def traverse(curr, currPath):
+            currPath += str(curr.val)
+
+            if not curr.left and not curr.right:
+                res.append(currPath)
+            
+            if curr.left:
+                traverse(curr.left, currPath + "->")
+
+            if curr.right:
+                traverse(curr.right, currPath + "->")
+
+        traverse(root, "")
+
         return res
-    
-    def traverse(self, curr, res, strs):        
-        if not curr.left and not curr.right:
-            res.append(strs + str(curr.val))
-            return
-        
-        if curr.left:
-            self.traverse(curr.left, res, strs + str(curr.val) + "->")
-        
-        if curr.right:
-            self.traverse(curr.right, res, strs + str(curr.val) + "->")
-                

abb197f42e970c2a5d0a2a404b5cd2a36f7c17a3
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-13 21:10:14 -0500

    Time: 225 ms (22.27%), Space: 30.3 MB (97.08%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,34 +1,39 @@
 from collections import deque
-
 class MyQueue:
+
     def __init__(self):
         self.queue = deque()
-        
-    def push(self, val: int):
-        while self.queue and val > self.queue[-1]:
+    
+    def push(self, x):
+        while self.queue and self.queue[-1] < x:
             self.queue.pop()
-        self.queue.append(val)
-        
-    def pop(self, val: int):
-        if self.queue and val == self.queue[0]:
-            self.queue.popleft()
-    
-    def front(self) -> int:
+
+        self.queue.append(x)
+
+    def front(self):
         return self.queue[0]
-        
-class Solution:
     
+
+    def pop(self, x):
+        if self.queue and x == self.queue[0]:
+            self.queue.popleft()
+
+
+class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        myQ = MyQueue()
+        queue = MyQueue()
+
         res = []
-        for i in range(k):
-            myQ.push(nums[i])
-        
-        res.append(myQ.front())
-        
-        for i in range(k, len(nums)):
-            myQ.pop(nums[i - k])
-            myQ.push(nums[i])
-            res.append(myQ.front())
-        
+
+        for idx, num in enumerate(nums):
+            queue.push(num)
+
+            if idx >= k:
+                queue.pop(nums[idx - k])
+
+            if idx >= k - 1:
+                res.append(queue.front())
+
+            
         return res
+

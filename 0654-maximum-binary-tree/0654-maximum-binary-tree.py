00e7d05142ae2da1b24b905659b80bc1662e98fd
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-14 16:57:45 -0500

    Time: 39 ms (30.48%), Space: 17.1 MB (46.26%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.py
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.py
@@ -5,21 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def findMaximum(self, arr):
+        max_idx, max_val = 0, arr[0]
+
+        for idx, val in enumerate(arr):
+            if val > max_val:
+                max_idx, max_val = idx, val
+
+        return max_idx, max_val
+
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-        def findMaximum(arr):
-            max_idx, max_val = 0, arr[0]
-
-            for idx, val in enumerate(arr):
-                if val > max_val:
-                    max_idx, max_val = idx, val
-
-            return max_idx, max_val
+        
         
         if not nums:
             return None
         
-        idx, val = findMaximum(nums)
-        
+        idx, val = self.findMaximum(nums)
+
         node = TreeNode(val)
 
         node.left = self.constructMaximumBinaryTree(nums[:idx])

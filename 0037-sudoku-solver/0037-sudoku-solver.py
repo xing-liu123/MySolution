e6b85ab622038e7a5a8e3b9cf876eb6548ef012d
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-16 13:31:18 -0500

    Time: 314 ms (18.75%), Space: 16.6 MB (67.92%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,38 +1,41 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         
-        def solve():
-            for i in range(9):
-                for j in range(9):
-                    if board[i][j] != '.':
-                        continue
-                    
-                    for k in range(1, 10):
-                        val = str(k)
-                        if isValid(i, j, val):
-                            board[i][j] = val
-                            if solve():
-                                return True
-                            board[i][j] = '.'
-                    
-                    return False
-            
-            return True
-        
-        def isValid(row, col, val) -> bool:
-            for i in range(9):
-                if board[i][col] == val:
-                    return False
-            
-            for j in range(9):
+        n = len(board)
+
+        def isValid(row: int, col: int, val: str) -> bool:
+            # Check row
+            for j in range(n):
                 if board[row][j] == val:
                     return False
-            
+
+            # Check column
+            for i in range(n):
+                if board[i][col] == val:
+                    return False
+
+            # Check square
             for i in range(row // 3 * 3, row // 3 * 3 + 3):
                 for j in range(col // 3 * 3, col // 3 * 3 + 3):
                     if board[i][j] == val:
                         return False
-            
+
+            return True
+
+        def solve():
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j] == ".":
+                        for val in range(1, 10):
+                            if isValid(i, j, str(val)):
+                                board[i][j] = str(val)
+                                if solve():
+                                    return True
+                                board[i][j] = "."
+                        return False
             return True
         
         solve()

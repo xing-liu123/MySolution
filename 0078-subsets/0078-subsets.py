a31c7278356f30ccfe75af0769b78ff19466c567
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-16 00:50:14 -0500

    Time: 0 ms (100%), Space: 16.8 MB (15.63%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,19 +1,16 @@
 class Solution:
+    def __init__(self):
+        self.path = []
+        self.res = []
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        path = []
-        res = []
+        self.helper(nums, 0)
+        return self.res
     
-        def backtracking(start: int):
-            res.append(path.copy())
-            
-            if start == len(nums):
-                return
-            
-            for i in range(start, len(nums)):
-                path.append(nums[i])
-                backtracking(i + 1)
-                path.pop()
-        
-        backtracking(0)
-        
-        return res
+    def helper(self, nums: List[int], start: int):
+        self.res.append(copy.copy(self.path))
+
+        for i in range(start, len(nums)):
+            self.path.append(nums[i])
+            self.helper(nums, i + 1)
+            self.path.pop()

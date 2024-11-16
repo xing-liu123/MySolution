842ba9eb179aa5e816cf6df7777295796644b9ae
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-15 23:28:56 -0500

    Time: 4 ms (5.37%), Space: 16.7 MB (36.9%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,24 +1,24 @@
 class Solution:
+    def __init__(self):
+        self.path = ""
+        self.res = []
+        self.map = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]] 
+
 
     def letterCombinations(self, digits: str) -> List[str]:
-        keys = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'], ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']]
-        
-        res = []
-        path = []
-        
-        if len(digits) < 1:
-            return res
-        
-        def backtracking(index):
-            if index == len(digits):
-                res.append(''.join(path))
-                return
+        if len(digits) == 0:
+            return self.res
             
-            for char in keys[int(digits[index]) - 2]:
-                path.append(char)
-                backtracking(index + 1)
-                path.pop()
-                
-        backtracking(0)
-        
-        return res
+        self.backtracking(digits, 0)
+        return self.res
+    
+    def backtracking(self, digits: str, idx: int):
+        if idx == len(digits):
+            self.res.append(copy.copy(self.path))
+            return
+
+
+        for d in self.map[int(digits[idx]) - 2]:
+            self.path += d
+            self.backtracking(digits, idx + 1)
+            self.path = self.path[:len(self.path) - 1]

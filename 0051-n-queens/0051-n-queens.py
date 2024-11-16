b3dd219ff1950f8909528ede6ace203682213fe7
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-16 12:48:08 -0500

    Time: 31 ms (34.23%), Space: 17.3 MB (5.68%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,47 +1,46 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = ['.'* n for _ in range(n)]
         res = []
-        
-        def backtracking(row):
-            if row == n:
-                res.append(board.copy())
+        board = [["."] * n for _ in range(n)]
+
+        def solve(currRow: int):
+            if currRow == n:
+                res.append([''.join(row) for row in board])
                 return
-            
+
             for col in range(n):
-                if isValid(row, col):
-                    board[row] = board[row][:col] + 'Q' + board[row][col + 1:]
-                    backtracking(row + 1)
-                    board[row] = board[row][:col] + '.' + board[row][col + 1:]
-            
-        
-        def isValid(row, col) -> bool:
+                if isValid(currRow, col):
+                    board[currRow][col] = "Q"
+                    solve(currRow + 1)
+                    board[currRow][col] = "."
+
+        def isValid(row: int, col: int) -> bool:
+            # Check this col
             for i in range(row):
-                if board[i][col] == 'Q':
+                if board[i][col] == "Q":
                     return False
-            
-            i, j = row - 1, col - 1
-            
+
+            # Check upper left
+            i = row - 1
+            j = col - 1
+
             while i >= 0 and j >= 0:
-                if board[i][j] == 'Q':
+                if board[i][j] == "Q":
                     return False
-                
                 i -= 1
                 j -= 1
-                
-            i, j = row - 1, col + 1
-            
+
+            # Check upper right
+            i = row - 1
+            j = col + 1
+
             while i >= 0 and j < n:
-                if board[i][j] == 'Q':
+                if board[i][j] == "Q":
                     return False
-                
                 i -= 1
                 j += 1
-            
-            return True
-        
-        backtracking(0)
-            
-        return res
 
-            
+            return True
+
+        solve(0)
+        return res

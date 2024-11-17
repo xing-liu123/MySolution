b06b60d41573e5e4bcda81ca82e04599ba605265
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-17 16:03:30 -0500

    Time: 3415 ms (18.03%), Space: 17.3 MB (95.51%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [sys.maxsize] * n
+        dp[0] = 0
+
+        for i in range(n - 1):
+            for j in range(i + 1, min(i + nums[i] + 1, n)):
+                dp[j] = min(dp[j], dp[i] + 1)
+
+        return dp[n - 1]
+

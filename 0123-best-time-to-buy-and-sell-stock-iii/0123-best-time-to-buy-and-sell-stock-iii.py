c3cbcc6af2993c195a60ecafc8e6c39fdbaef293
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-19 13:34:07 -0500

    Time: 479 ms (34.28%), Space: 41.3 MB (27.19%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[0] * 5 for _ in range(n)]
+        dp = [[0] * 4 for _ in range(n)]
         
-        dp[0][1] = -prices[0] # first hold
-        dp[0][3] = -prices[0]
+        dp[0][0] = -prices[0] # first hold
+        dp[0][2] = -prices[0]
 
         for i in range(1, n):
-            dp[i][0] = dp[i - 1][0]
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
-            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] + prices[i])
-            dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] - prices[i])
-            dp[i][4] = max(dp[i - 1][4], dp[i - 1][3] + prices[i])
+            dp[i][0] = max(dp[i - 1][0], -prices[i])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] - prices[i])
+            dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] + prices[i])
 
         return max(dp[-1])

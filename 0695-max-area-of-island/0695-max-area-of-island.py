f5535aa1011b1deb4210b5c39349e0dd1674aabd
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-20 15:21:03 -0500

    Time: 22 ms (62.33%), Space: 17.6 MB (37.78%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,29 +1,30 @@
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        visited = [[False] * n for _ in range(m)]
-        max_area = 0
-        dr = [0, 0, 1, -1]
-        dc = [1, -1, 0, 0]
+    
+    def __init__(self):
+        self.res = 0
+        self.currMax = 0   
 
-        def dfs(row, col):
-            nonlocal max_area
-            nonlocal curr_area
-            visited[row][col] = True
-            curr_area += 1
-            max_area = max(max_area, curr_area)
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dr = [1, -1, 0, 0]
+        dc = [0, 0, 1, -1]
+
+        def dfs(x, y):
+            grid[x][y] = 0
+            self.currMax += 1
 
             for k in range(4):
-                x = row + dr[k]
-                y = col + dc[k]
+                nx, ny = x + dr[k], y + dc[k]
 
-                if x >= 0 and x < m and y >= 0 and y < n and not visited[x][y] and grid[x][y] == 1:
-                    dfs(x, y)
-        
+                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+                    dfs(nx, ny)
+
         for i in range(m):
             for j in range(n):
-                if not visited[i][j] and grid[i][j] == 1:
-                    curr_area = 0
+                if grid[i][j] == 1:
                     dfs(i, j)
-        
-        return max_area
+                    self.res = max(self.res, self.currMax)
+                    self.currMax = 0
+
+        return self.res

289e1e850d69da9e4f7f4c605fecc30b4ce00926
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-20 14:52:25 -0500

    Time: 4 ms (65.62%), Space: 18 MB (73.01%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,19 +1,17 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        res = []
         path = [0]
-        target = len(graph) - 1
+        res = []
 
-        def backtracking(curr):
-            if curr == target:
-                res.append(path.copy())
+        def dfs(curr):
+            if curr == len(graph) - 1:
+                res.append(copy.copy(path))
                 return
             
-            for node in graph[curr]:
-                path.append(node)
-                backtracking(node)
+            for edge in graph[curr]:
+                path.append(edge)
+                dfs(edge)
                 path.pop()
 
-        backtracking(0)
-
+        dfs(0)
         return res

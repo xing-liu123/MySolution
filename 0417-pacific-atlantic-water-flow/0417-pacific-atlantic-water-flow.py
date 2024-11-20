bfecce7502483a295a4d2bd0cd67e9c44d9d2979
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-20 16:44:19 -0500

    Time: 203 ms (11.55%), Space: 18.7 MB (10.49%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -6,59 +6,31 @@ class Solution:
         m, n = len(heights), len(heights[0])
 
         pacific = [[False] * n for _ in range(m)]
-
-        for i in range(m):
-            pacific[i][0] = True
-        
-        for j in range(n):
-            pacific[0][j] = True
-        
         atlantic = [[False] * n for _ in range(m)]
+        
+        def dfs(row, col, visited):
+            visited[row][col] = True
+
+            for k in range(4):
+                x, y = row + dr[k], col + dc[k]
+
+                if 0 <= x < m and 0 <= y < n and not visited[x][y]:
+                    if heights[x][y] >= heights[row][col]:
+                        visited[x][y] = True
+                        dfs(x, y, visited)
 
         for i in range(m):
-            atlantic[i][-1] = True
+            dfs(i, 0, pacific)
+            dfs(i, n - 1, atlantic)
         
         for j in range(n):
-            atlantic[-1][j] = True
+            dfs(0, j, pacific)
+            dfs(m - 1, j, atlantic)
 
-        visited = [[False] * n for _ in range(m)]
-
-        for i in range(m):
-            if not visited[i][0]:
-                self.dfs(i, 0, pacific, dr, dc, heights, visited)
-        
-        for j in range(n):
-            if not visited[0][j]:
-                self.dfs(0, j, pacific, dr, dc, heights, visited)
-
-        visited = [[False] * n for _ in range(m)]
-
-        for i in range(m):
-            if not visited[i][n - 1]:
-                self.dfs(i, n - 1, atlantic, dr, dc, heights, visited)
-
-        
-        for j in range(n):
-            if not visited[m - 1][j]:
-                self.dfs(m - 1, j, atlantic, dr, dc, heights, visited)
-
-        res = []
-
-        for i in range(m):
-            for j in range(n):
-                if pacific[i][j] and atlantic[i][j]:
-                    res.append([i, j])
+        res = [[i, j] for i in range(m) for j in range(n) if pacific[i][j] and atlantic[i][j]]
         
         return res
 
-    def dfs(self, row, col, grid, dr, dc, heights, visited):
-        visited[row][col] = True
-        for k in range(4):
-            x, y = row + dr[k], col + dc[k]
-
-            if x >= 0 and x < len(heights) and y >= 0 and y < len(heights[0]) and not visited[x][y]:
-                if heights[x][y] >= heights[row][col]:
-                    grid[x][y] = True
-                    self.dfs(x, y, grid, dr, dc, heights, visited)
+    
 
         

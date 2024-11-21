1cec34f99cb108919fe66e9fb682be492cf41ae9
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-11-21 01:46:25 -0500

    Time: 600 ms (94.4%), Space: 28.2 MB (48.59%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,53 +1,55 @@
 class Solution:
-    def __init__(self):
-        self.areas = defaultdict(int)
-        self.grid = []
-        self.dr = [1, -1, 0, 0]
-        self.dc = [0, 0, 1, -1]
-        self.m = 0
-        self.n = 0
-        self.visited = []
-
     def largestIsland(self, grid: List[List[int]]) -> int:
-        self.grid = grid
-        self.m, self.n = len(grid), len(grid[0])
-        self.visited = [[False] * self.n for _ in range(self.m)]
+        currArea = 0
+        maxArea = 0
+
+        m, n = len(grid), len(grid[0])
+        dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
+
+        def dfs(x, y, marker):
+            nonlocal currArea
+            currArea += 1
+            grid[x][y] = marker
+
+            for k in range(4):
+                nx, ny = x + dr[k], y + dc[k]
+
+                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+                    dfs(nx, ny, marker)
+
         marker = 2
-          
-        for i in range(self.m):
-            for j in range(self.n):
-                if self.grid[i][j] == 1:
-                    self.dfs(i, j, marker)
+        areas = {}
+        for i in range(m):
+            for j in range(n):
+
+                if grid[i][j] == 1:
+                    currArea = 0
+                    dfs(i, j, marker)
+                    areas[marker] = currArea
                     marker += 1
 
-        largest = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    neighbors = set()
+                    if i > 0 and grid[i - 1][j] > 0:
+                        neighbors.add(grid[i - 1][j])
 
-        for i in range(self.m):
-            for j in range(self.n):
-                if self.grid[i][j] == 0:
-                    visited = set()
+                    if i < m - 1 and grid[i + 1][j] > 0:
+                        neighbors.add(grid[i + 1][j])
+
+                    if j > 0 and grid[i][j - 1] > 0:
+                        neighbors.add(grid[i][j - 1])
+
+                    if j < n - 1 and grid[i][j + 1] > 0:
+                        neighbors.add(grid[i][j + 1])
+
                     area = 1
-                    for k in range(4):
-                        x, y = i + self.dr[k], j + self.dc[k]
 
-                        if 0 <= x < self.m and 0 <= y < self.n and self.grid[x][y] > 1:
-                            if grid[x][y] not in visited:
-                                area += self.areas[grid[x][y]]
-                                visited.add(grid[x][y])
-                                
-                    largest = max(largest, area)
-                
-        return largest if largest != 0 else self.m * self.n
+                    for nei in neighbors:
+                        area += areas.get(nei, 0)
 
-    def dfs(self, row, col, marker):
-            self.grid[row][col] = marker
-            self.areas[marker] += 1
-            for k in range(4):
-                x, y = row + self.dr[k], col + self.dc[k]
+                    maxArea = max(maxArea, area)
+                    
 
-                if 0 <= x < self.m and 0 <= y < self.n and self.grid[x][y] == 1:
-                    self.dfs(x, y, marker)
-
-
-
-    
+        return maxArea if maxArea > 0 else m * n

485fa0d942351e0ef6a798673e881d68ccc4114e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-12-28 00:05:29 -0500

    Time: 45 ms (58.44%), Space: 25.3 MB (6.35%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.py
+++ 1166-design-file-system/1166-design-file-system.py
@@ -10,32 +10,36 @@ class FileSystem:
         self.root = TrieNode("", -1)
 
     def createPath(self, path: str, value: int) -> bool:
-        curr = self.root
-        node = path.split("/")[1:]
-        curr_idx = 0
+        if path[0] != "/" or path == "/":
+            return False
 
-        while curr_idx < len(node) and node[curr_idx] in curr.children:
-            curr = curr.children[node[curr_idx]]
-            curr_idx += 1
+        curr = self.root
+        nodes = path.split("/")[1:]
+
+        for i in range(len(nodes) - 1):
+            if not nodes[i] in curr.children:
+                return False
+            
+            curr = curr.children[nodes[i]]
         
-        if curr_idx != len(node) - 1:
+        if nodes[-1] in curr.children:
             return False
         
-        curr.children[node[curr_idx]] = TrieNode(node[-1], value)
+        curr.children[nodes[-1]] = TrieNode(nodes[-1], value)
+
         return True
 
     def get(self, path: str) -> int:
         curr = self.root
-        node = path.split("/")[1:]
+        nodes = path.split("/")[1:]
         curr_idx = 0
 
-        while curr_idx < len(node) and node[curr_idx] in curr.children:
-            curr = curr.children[node[curr_idx]]
-            curr_idx += 1
+        for node in nodes:
+            if not node in curr.children:
+                return -1
+            
+            curr = curr.children[node]
         
-        if curr_idx != len(node):
-            return -1
-
         return curr.val
 
 

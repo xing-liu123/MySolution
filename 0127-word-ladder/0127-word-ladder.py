6462ed6ee804818ffd7645e6331017a775b0e48f
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-12-31 15:40:26 -0700

    Time: 295 ms (47.69%), Space: 18.8 MB (60.51%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,26 +2,32 @@ from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         word_set = set(wordList)
-        checked_word = set()
 
         if not endWord in word_set:
             return 0
 
-        queue = deque([(beginWord, 1)])
+        alphabet = string.ascii_lowercase
+
+        queue = deque([beginWord])
+        length = 1
+
         
-
         while queue:
-            word, length = queue.popleft()
-            checked_word.add(word)
+            length += 1
+            size = len(queue)
+            
+            for _ in range(size):
+                word = queue.popleft()
 
-            for i in range(len(word)):
-                for j in range(26):
-                    next_word = word[:i] + chr(ord('a') + j) + word[i + 1:]
+                for i in range(len(word)):
+                    for c in alphabet:
+                        next_word = word[:i] + c + word[i + 1:]
 
-                    if next_word == endWord:
-                        return length + 1
+                        if next_word == endWord:
+                            return length
 
-                    if not next_word in checked_word and next_word in word_set:
-                        queue.append((next_word, length + 1))
+                        if next_word in word_set:
+                            word_set.remove(next_word)
+                            queue.append(next_word)
 
         return 0

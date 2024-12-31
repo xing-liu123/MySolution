054232a45ae366c2298d260f558194bf224d83c9
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2024-12-31 15:21:50 -0700

    Time: 87 ms (58.72%), Space: 26.9 MB (18.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        x = 0
-        y = -prices[0] # hold stock
+        max_profit = 0
+        curr_min = prices[0]
 
         for i in range(1, n):
-            x = max(y + prices[i], x)
-            y = max(-prices[i], y) 
+            if prices[i] >= curr_min:
+                max_profit = max(max_profit, prices[i] - curr_min)
+            else:
+                curr_min = prices[i]
 
-        return max(x, y)
+        return max_profit

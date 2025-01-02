dec32cf05793bd6f01669b2a81007b7deb5d1d1c
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-01 21:00:15 -0700

    Time: 219 ms (91.67%), Space: 18.1 MB (21.38%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,22 +1,52 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # n = len(s)
+        # dp = [[False] * n for _ in range(n)]
+
+        # maxLen = 1
+        # res = s[0]
+
+        # for i in range(n - 1, -1, -1):
+        #     for j in range(i, n, 1):
+        #         if s[i] == s[j]:
+        #             if j - i <= 1 or dp[i + 1][j - 1]:
+        #                 dp[i][j] = True
+
+        #                 if j - i + 1 > maxLen:
+        #                     maxLen = j - i + 1
+        #                     res = s[i: j + 1]          
+
+        # return res
+
         n = len(s)
-        dp = [[False] * n for _ in range(n)]
 
-        maxLen = 1
-        res = s[0]
+        def expand(left, right):
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
 
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n, 1):
-                if s[i] == s[j]:
-                    if j - i <= 1 or dp[i + 1][j - 1]:
-                        dp[i][j] = True
+            return right - left - 1, left + 1, right - 1
 
-                        if j - i + 1 > maxLen:
-                            maxLen = j - i + 1
-                            res = s[i: j + 1]          
+        maxLen, maxLeft, maxRight = 1, 0, 0
 
-        return res
+        for i in range(n):
+            oddLen, oddLeft, oddRight = expand(i, i)
+
+            if oddLen > maxLen:
+                maxLen, maxLeft, maxRight = oddLen, oddLeft, oddRight
+
+            evenLen, evenLeft, evenRight = expand(i, i + 1)
+
+            if evenLen > maxLen:
+                maxLen, maxLeft, maxRight = evenLen, evenLeft, evenRight
+
+        return s[maxLeft: maxRight + 1]
+
+
+
+
+
+            
 
 
 

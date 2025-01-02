013790bbe9ff844cc8a145c0a9e8b9f8276b4297
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-01 21:21:36 -0700

    Time: 555 ms (70.04%), Space: 20.7 MB (27.46%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,67 +1,38 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+
         nums.sort()
 
         res = []
-        n = len(nums)
+
+        if nums[0] > 0:
+            return res
 
         for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i - 1] == nums[i]:
                 continue
-            
+
             left = i + 1
             right = n - 1
 
             while left < right:
-                currSum = nums[i] + nums[left] + nums[right]
+                three_sum = nums[i] + nums[left] + nums[right]
 
-                if currSum < 0:
-                    left += 1
-                elif currSum > 0:
+                if three_sum > 0:
                     right -= 1
+                elif three_sum < 0:
+                    left += 1
                 else:
                     res.append([nums[i], nums[left], nums[right]])
 
-                    while left < right and nums[left + 1] == nums[left]:
+                    while left < right and nums[left] == nums[left + 1]:
                         left += 1
 
-                    while left < right and nums[right - 1] == nums[right]:
+                    while left < right and nums[right] == nums[right - 1]:
                         right -= 1
 
                     left += 1
                     right -= 1
-
-        return res
-                
-
-        # res = []
-        # nums = sorted(nums)
-
-        # for i in range(len(nums)):
-        #     if nums[0] > 0:
-        #         return res
-            
-        #     if i > 0 and nums[i] == nums[i - 1]:
-        #         continue
-            
-        #     left = i + 1
-        #     right = len(nums) - 1
-        #     while left < right:
-        #         sum = nums[i] + nums[left] + nums[right]
-
-        #         if sum > 0:
-        #             right -= 1
-        #         elif sum < 0:
-        #             left += 1
-        #         else:
-        #             res.append([nums[i], nums[left], nums[right]])
-        #             while left < right and nums[left + 1] == nums[left]:
-        #                 left += 1   
-        #             while left < right and nums[right - 1] == nums[right]:
-        #                 right -= 1
-                    
-        #             left += 1
-        #             right -= 1
         
-
-        # return res
+        return res

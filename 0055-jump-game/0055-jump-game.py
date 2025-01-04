90d859ffd8ac54bb4415c4391fc383ef4dbbd063
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-04 12:48:30 -0700

    Time: 24 ms (56.12%), Space: 18.6 MB (28.73%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,13 +1,14 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-        curr_idx = 0
         curr_max = 0
 
-        while curr_idx <= curr_max:
-            curr_max = max(curr_max, curr_idx + nums[curr_idx])
-            curr_idx += 1
-            if curr_max >= n - 1:
+        for i in range(len(nums)):
+            if i > curr_max:
+                return False
+
+            curr_max = max(curr_max, i + nums[i])
+
+            if curr_max >= len(nums) - 1:
                 return True
 
-        return curr_max >= n - 1
+        return False

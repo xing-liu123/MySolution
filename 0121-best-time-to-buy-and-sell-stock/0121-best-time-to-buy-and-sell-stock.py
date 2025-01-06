ddfe8241f4e137d464470ea34020832892d7a4f2
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-06 12:12:25 -0700

    Time: 130 ms (25%), Space: 27 MB (11.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        max_profit = 0
         curr_min = prices[0]
+        max_profit = 0
 
-        for i in range(1, n):
-            if prices[i] >= curr_min:
-                max_profit = max(max_profit, prices[i] - curr_min)
-            else:
-                curr_min = prices[i]
+        for i in range(len(prices)):
+            max_profit = max(max_profit, prices[i] - curr_min)
+            curr_min = min(curr_min, prices[i])
 
         return max_profit

d75723922728340904ffc1d6540c43ab641ee03c
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-08 11:31:00 -0700

    Time: 2394 ms (6.84%), Space: 17.4 MB (99.61%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,24 +1,22 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        char_indices = defaultdict(list)
+        left = defaultdict(lambda: 0)
+        left[s[0]] = 1
 
-        for idx, char in enumerate(s):
-            char_indices[char].append(idx)
+        right = defaultdict(lambda: 0)
+        res = set()
 
-        count = 0
+        for char in s[1:]:
+            right[char] += 1
 
-        for indice_list in char_indices.values():
-            if len(indice_list) > 1:
-                left, right = indice_list[0], indice_list[-1]
-                char_set = set()
-
-                for i in range(left + 1, right):
-                    char_set.add(s[i])
-
-                count += len(char_set)
-
-        return count
-
+        for char in s[1:]:
+            right[char] -= 1
+            for c in left.keys():
+                if left[c] > 0 and right[c] > 0:
+                    word = c + char + c
+                    if not word in res:
+                        res.add(word)
+            left[char] += 1
         
+        return len(res)
 
-        

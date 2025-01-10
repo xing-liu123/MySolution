238ebef32852ca88633629521fc6ffd5e3533a1e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-10 15:04:50 -0700

    Time: 0 ms (100%), Space: 17.8 MB (26.53%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        isNeg = n < 0
+        n = abs(n)
+
+        def modular_exponentation(base, exp):
+            res = 1
+
+            while exp > 0:
+                if exp % 2 == 1:
+                    res *= base
+                base *= base
+                exp //= 2
+
+            return res
+
+        res = modular_exponentation(x, n)
+
+        return 1 / res if isNeg else res

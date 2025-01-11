d17ac30418d1666a4c5eccd67e90d5773f0cdae7
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-11 14:44:34 -0700

    Time: 106 ms (21.6%), Space: 32.8 MB (26.43%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
@@ -1,13 +1,16 @@
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
         remainder_counts = defaultdict(int)
+        leftover = 0
 
         for num in arr:
             remainder = num % k
 
             if remainder_counts[(k - remainder) % k] > 0:
                 remainder_counts[(k - remainder) % k] -= 1
+                leftover -= 1
             else:
                 remainder_counts[remainder] += 1
+                leftover += 1
 
-        return sum(remainder_counts.values()) == 0
+        return leftover == 0

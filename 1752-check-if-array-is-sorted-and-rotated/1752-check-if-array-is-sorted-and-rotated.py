3062e4d2819b0b7fdb76493d0fedbb6a34057a9b
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-11 18:43:44 -0700

    Time: 0 ms (100%), Space: 17.6 MB (41.98%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,21 +1,13 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        rotate_found = False
-        min_val, max_val = nums[0], nums[0]
+        count = 0
 
-        for i in range(1, len(nums)):
-            if nums[i] < nums[i - 1]:
-                if rotate_found:
-                    return False
-                else:
-                    rotate_found = True
+        for i in range(len(nums)):
+            if nums[(i + 1) % len(nums)] < nums[i]:
+                count += 1
             
-            elif not rotate_found:
-                max_val = nums[i]
-            
-            if rotate_found:
-                if min_val == max_val and nums[i] > max_val or min_val != max_val and min_val < nums[i] < max_val:
-                    return False
+            if count > 1:
+                return False
             
 
         return True

c58dd170c29b82245f5fab46ad055f69f7ba3bdc
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-13 12:48:30 -0700

    Time: 515 ms (15.95%), Space: 21.3 MB (52.11%) - LeetHub

--- 0916-word-subsets/0916-word-subsets.py
+++ 0916-word-subsets/0916-word-subsets.py
@@ -1,26 +1,25 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        word_counts2 = {}
+        max_freq = [0] * 26
 
         for word in words2:
-            word_counts2[word] = Counter(word)
+            counts = Counter(word)
 
-        def isUniversal(s1):
-            
-            count1 = Counter(s1)
-
-            for count in word_counts2.values():
-                for char in count.keys():
-                    if count1[char] < count[char]:
-                        return False
-
-            return True
+            for char in counts.keys():
+                idx = ord(char) - ord('a')
+                max_freq[idx] = max(max_freq[idx], counts[char])
 
         res = []
-        
-        for w1 in words1:
-            if isUniversal(w1):
-                res.append(w1)
+
+        for word in words1:
+            counts = Counter(word)
+            isUniversal = True
+            for i in range(26):
+                char = chr(i + ord("a"))   
+                if counts[char] < max_freq[i]:
+                    isUniversal = False
+                    break
+            if isUniversal:
+                res.append(word)
 
         return res
-            

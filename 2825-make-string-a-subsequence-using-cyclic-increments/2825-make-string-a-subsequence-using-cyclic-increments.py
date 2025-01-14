58db09545537edffc2a021017b264a8e5bfdc5ce
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-13 18:11:47 -0700

    Time: 123 ms (10.82%), Space: 18.3 MB (7.34%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        char_counts = Counter(str1)
+
+        idx1 = idx2 = 0
+
+        while idx1 < len(str1) and idx2 < len(str2):
+            if str1[idx1] == str2[idx2] or ord(str1[idx1]) - ord(str2[idx2]) == -1 or str1[idx1] == "z" and str2[idx2] == "a":
+                idx2 += 1
+
+            idx1 += 1
+
+        return idx2 == len(str2)
+            
+

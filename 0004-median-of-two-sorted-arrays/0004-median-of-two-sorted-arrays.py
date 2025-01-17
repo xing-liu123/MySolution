fbc272ae7ae6d97775336e4e247e642c25a93538
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-16 18:18:01 -0700

    Time: 3 ms (54.15%), Space: 18.2 MB (11.94%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,34 +1,63 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # if len(nums1) > len(nums2):
+        #     nums1, nums2 = nums2, nums1
+
+        # m, n = len(nums1), len(nums2)
+        # low, high = 0, m
+
+        # while low <= high:
+        #     partition1 = (low + high) //2
+        #     partition2 = (m + n + 1) // 2 - partition1
+
+        #     maxLeft1 = float('-inf') if partition1 == 0 else nums1[partition1 - 1]
+        #     minRight1 = float('inf') if partition1 == m else nums1[partition1]
+
+        #     # Left and right boundaries of partition in nums2
+        #     maxLeft2 = float('-inf') if partition2 == 0 else nums2[partition2 - 1]
+        #     minRight2 = float('inf') if partition2 == n else nums2[partition2]
+
+        #     # Check if we found the correct partition
+        #     if maxLeft1 <= minRight2 and maxLeft2 <= minRight1:
+        #         # If total number of elements is odd
+        #         if (m + n) % 2 == 1:
+        #             return max(maxLeft1, maxLeft2)
+        #         else:
+        #             return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2
+        #     # Move search space to the left in nums1
+        #     elif maxLeft1 > minRight2:
+        #         high = partition1 - 1
+        #     # Move search space to the right in nums1
+        #     else:
+        #         low = partition1 + 1
+
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
 
         m, n = len(nums1), len(nums2)
+
         low, high = 0, m
 
         while low <= high:
-            partition1 = (low + high) //2
-            partition2 = (m + n + 1) // 2 - partition1
+            p1 = (low + high) // 2
+            p2 = (m + n + 1) // 2 - p1
 
-            maxLeft1 = float('-inf') if partition1 == 0 else nums1[partition1 - 1]
-            minRight1 = float('inf') if partition1 == m else nums1[partition1]
+            max_left1 = float("-inf") if p1 == 0 else nums1[p1 - 1]
+            min_right1 = float("inf") if p1 == m else nums1[p1]
 
-            # Left and right boundaries of partition in nums2
-            maxLeft2 = float('-inf') if partition2 == 0 else nums2[partition2 - 1]
-            minRight2 = float('inf') if partition2 == n else nums2[partition2]
+            max_left2 = float("-inf") if p2 == 0 else nums2[p2 - 1]
+            min_right2 = float("inf") if p2 == n else nums2[p2]
 
-            # Check if we found the correct partition
-            if maxLeft1 <= minRight2 and maxLeft2 <= minRight1:
-                # If total number of elements is odd
+            if max_left1 <= min_right2 and max_left2 <= min_right1:
                 if (m + n) % 2 == 1:
-                    return max(maxLeft1, maxLeft2)
+                    return max(max_left1, max_left2)
                 else:
-                    return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2
-            # Move search space to the left in nums1
-            elif maxLeft1 > minRight2:
-                high = partition1 - 1
-            # Move search space to the right in nums1
+                    return (max(max_left1, max_left2) + min(min_right1, min_right2)) / 2
+
+            elif max_left1 > min_right2:
+                high = p1 - 1
             else:
-                low = partition1 + 1
+                low = p1 + 1            
+
 
 

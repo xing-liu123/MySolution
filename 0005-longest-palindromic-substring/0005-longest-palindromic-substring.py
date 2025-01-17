e7edb56ca37ad29ab888ee73a8603a703e1cc948
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-16 19:17:53 -0700

    Time: 212 ms (92.93%), Space: 17.7 MB (53.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,49 +1,49 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
-        # n = len(s)
-
-        # def expand(left, right):
-        #     while left >= 0 and right < n and s[left] == s[right]:
-        #         left -= 1
-        #         right += 1
-
-        #     return right - left - 1, left + 1, right - 1
-
-        # maxLen, maxLeft, maxRight = 1, 0, 0
-
-        # for i in range(n):
-        #     oddLen, oddLeft, oddRight = expand(i, i)
-
-        #     if oddLen > maxLen:
-        #         maxLen, maxLeft, maxRight = oddLen, oddLeft, oddRight
-
-        #     evenLen, evenLeft, evenRight = expand(i, i + 1)
-
-        #     if evenLen > maxLen:
-        #         maxLen, maxLeft, maxRight = evenLen, evenLeft, evenRight
-
-        # return s[maxLeft: maxRight + 1]
-
         n = len(s)
 
-        dp = [[False] * n for _ in range(n)]
+        def expand(left, right):
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+
+            return right - left - 1, left + 1, right - 1
+
+        maxLen, maxLeft, maxRight = 1, 0, 0
 
         for i in range(n):
-            dp[i][i] = True
+            oddLen, oddLeft, oddRight = expand(i, i)
 
-        left, right = 0, 0
+            if oddLen > maxLen:
+                maxLen, maxLeft, maxRight = oddLen, oddLeft, oddRight
 
-        for i in range(n - 1, -1, -1):
-            for j in range(i + 1, n):
-                if s[i] == s[j]:
-                    if i + 1 == j or dp[i + 1][j - 1]:
-                        dp[i][j] = True
+            evenLen, evenLeft, evenRight = expand(i, i + 1)
 
-                        if j - i > right - left:
-                            left, right = i, j
+            if evenLen > maxLen:
+                maxLen, maxLeft, maxRight = evenLen, evenLeft, evenRight
 
-        return s[left: right + 1]
+        return s[maxLeft: maxRight + 1]
+
+        # n = len(s)
+
+        # dp = [[False] * n for _ in range(n)]
+
+        # for i in range(n):
+        #     dp[i][i] = True
+
+        # left, right = 0, 0
+
+        # for i in range(n - 1, -1, -1):
+        #     for j in range(i + 1, n):
+        #         if s[i] == s[j]:
+        #             if i + 1 == j or dp[i + 1][j - 1]:
+        #                 dp[i][j] = True
+
+        #                 if j - i > right - left:
+        #                     left, right = i, j
+
+        # return s[left: right + 1]
 
 
         # n = len(s)

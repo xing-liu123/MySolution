5b5722b2b136c7aeace7777deb5867b280cc80c5
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-17 11:25:36 -0700

    Time: 8 ms (37.38%), Space: 18.1 MB (30.8%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,26 +1,24 @@
 class Solution:
-    def __init__(self):
-        self.path = []
-        self.res = []
-        self.used = []
+   
 
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        self.used = [0] * len(nums)
+        path = []
+        res = []
+        used = [False] * len(nums)
         nums.sort()
-        self.backtracking(nums)
-        return self.res
-    
-    def backtracking(self, nums: List[int]):
-        if len(self.path) == len(nums):
-            self.res.append(copy.copy(self.path))
-        
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1] and self.used[i - 1] == 0:
-                continue
 
-            if self.used[i] == 0:
-                self.used[i] = 1
-                self.path.append(nums[i])
-                self.backtracking(nums)
-                self.used[i] = 0
-                self.path.pop()
+        def backtracking():
+            if len(path) == len(nums):
+                res.append(copy.copy(path))
+                return
+            
+            for i in range(len(nums)):
+                if not used[i] and (i == 0 or i > 0 and nums[i] != nums[i - 1] or used[i - 1]):
+                    path.append(nums[i])
+                    used[i] = True
+                    backtracking()
+                    used[i] = False
+                    path.pop()
+
+        backtracking()
+        return res

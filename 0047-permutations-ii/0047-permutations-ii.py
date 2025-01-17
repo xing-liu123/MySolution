e567a4dbd143a3f56ce07fcc15068fde47602b71
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-17 11:28:18 -0700

    Time: 23 ms (24.75%), Space: 18.2 MB (11.74%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -13,7 +13,7 @@ class Solution:
                 return
             
             for i in range(len(nums)):
-                if not used[i] and (i == 0 or i > 0 and nums[i] != nums[i - 1] or used[i - 1]):
+                if not used[i] and (i == 0 or i > 0 and nums[i] != nums[i - 1] or not used[i - 1]):
                     path.append(nums[i])
                     used[i] = True
                     backtracking()

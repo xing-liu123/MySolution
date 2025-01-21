6347d011a0ddb1917eabe06c7af251077de7e64e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-20 22:52:43 -0700

    Time: 0 ms (100%), Space: 17.8 MB (67.03%) - LeetHub

--- 3407-substring-matching-pattern/3407-substring-matching-pattern.py
+++ 3407-substring-matching-pattern/3407-substring-matching-pattern.py
@@ -1,11 +1,11 @@
 class Solution:
     def hasMatch(self, s: str, p: str) -> bool:
-        left, right = p.split("*")
+        left,right = p.split("*")
 
         left_idx = s.find(left)
         right_idx = s.rfind(right)
 
         if left_idx == -1 or right_idx == -1:
             return False
-
-        return left_idx + len(left) < right_idx
+        
+        return left_idx + len(left) <= right_idx

5813af6d6b92ed42171ee4d5569d8781692661a8
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-22 00:28:03 -0700

    Time: 56 ms (30.39%), Space: 49.1 MB (38.82%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -5,8 +5,12 @@
  */
 var map = function(arr, fn) {
     const newArr = new Array(arr.length);
-    for (const i in arr) {
-        newArr[i] = fn(arr[i], Number(i));
+    // for (const i in arr) {
+    //     newArr[i] = fn(arr[i], Number(i));
+    // }
+
+    for (let i = 0; i < arr.length; i++) {
+        newArr[i] = fn(arr[i], i)
     }
     return newArr;
 };

bc13a8f958ad9384d334dad78facc19ed29883f7
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-23 18:19:49 -0500

    Time: 67 ms (39.57%), Space: 50.2 MB (60.36%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -4,12 +4,11 @@
  */
 var compose = function(functions) {
     return function(x) {
-        let res = x;
-        for(let i = functions.length - 1; i >= 0; i--) {
-            res = functions[i](res);
-        }
+        functions.reverse().forEach(fn => {
+            x = fn(x)
+        })
 
-        return res;
+        return x
     }
 };
 

39d681d47d491fb89a0f38e2df8b5098dc19d07c
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-24 22:03:15 -0500

    Time: 0 ms (100%), Space: 18 MB (18.32%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,41 +1,91 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
+        # m, n = len(grid), len(grid[0])
 
+        # dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
+
+        # queue = deque()
+        # fresh_count = 0
+        # rotten_count = 0
+
+        # for i in range(m):
+        #     for j in range(n):
+        #         if grid[i][j] == 2:
+        #             queue.append((i, j))
+        #             rotten_count += 1
+        #         elif grid[i][j] == 1:
+        #             fresh_count += 1
+
+        # if fresh_count == 0:
+        #     return 0
+
+        # if fresh_count > 0 and rotten_count == 0:
+        #     return -1
+        
+        # time = -1
+        # current_count = 0
+
+        # while queue:
+        #     size = len(queue)
+        #     current_count += size
+        #     time += 1
+
+        #     for _ in range(size):
+        #         i, j = queue.popleft()
+
+        #         for k in range(4):
+        #             next_i = i + dr[k]
+        #             next_j = j + dc[k]
+
+        #             if 0 <= next_i < m and 0 <= next_j < n and grid[next_i][next_j] == 1:
+        #                 grid[next_i][next_j] = 2
+        #                 queue.append((next_i, next_j))
+
+        # if current_count < rotten_count + fresh_count:
+        #     return -1
+
+        # return time
+
+        m, n = len(grid), len(grid[0])
         dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
+        
+        rotten_count = 0
+        fresh_count = 0
 
         queue = deque()
-        orange_count = 0
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 2:
+                if grid[i][j] == 1:
+                    fresh_count += 1
+
+                elif grid[i][j] == 2:
+                    rotten_count += 1
                     queue.append((i, j))
 
-                if grid[i][j] != 0:
-                    orange_count += 1
+        if fresh_count == 0:
+            return 0
+        
+        if rotten_count == 0:
+            return -1
 
         time = -1
-        current_count = 0
 
         while queue:
             size = len(queue)
-            current_count += size
             time += 1
 
             for _ in range(size):
-                i, j = queue.popleft()
+                row, col = queue.popleft()
 
                 for k in range(4):
-                    next_i = i + dr[k]
-                    next_j = j + dc[k]
+                    nr = row + dr[k]
+                    nc = col + dc[k]
 
-                    if 0 <= next_i < m and 0 <= next_j < n and grid[next_i][next_j] == 1:
-                        grid[next_i][next_j] = 2
-                        queue.append((next_i, next_j))
+                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
+                        queue.append((nr, nc))
+                        grid[nr][nc] = 2
+                        fresh_count -= 1
 
-        if current_count < orange_count:
-            return -1
-
-        return time
+        return time if fresh_count == 0 else -1

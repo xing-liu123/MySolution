aa020ce0b18d35f7e8e6c370f8f6f31be919e8c8
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-29 23:53:41 -0500

    Time: 0 ms (100%), Space: 17.7 MB (41.5%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -5,25 +5,48 @@ class Solution:
         """
         n = len(nums)
 
-        i = n - 2
+        i = n - 1
 
-        while i >= 0 and nums[i] >= nums[i + 1]:
+        while i > 0 and nums[i] <= nums[i - 1]:
             i -= 1
 
-        if i >= 0:
-            j = n - 1
+        if i == 0:
+            nums.sort()
+            return
 
-            while nums[j] <= nums[i]:
-                j -= 1
+        idxToSwap = i
+
+        for k in range(i, n):
+            if nums[k] > nums[i - 1] and nums[k] <= nums[idxToSwap]:
+                idxToSwap = k
+
+        nums[i - 1], nums[idxToSwap] = nums[idxToSwap], nums[i - 1]
+
+        nums[i:] = sorted(nums[i:])
+
+
+
+        # n = len(nums)
+
+        # i = n - 2
+
+        # while i >= 0 and nums[i] >= nums[i + 1]:
+        #     i -= 1
+
+        # if i >= 0:
+        #     j = n - 1
+
+        #     while nums[j] <= nums[i]:
+        #         j -= 1
             
-            nums[i], nums[j] = nums[j], nums[i]
+        #     nums[i], nums[j] = nums[j], nums[i]
 
-        left, right = i + 1, n - 1
+        # left, right = i + 1, n - 1
 
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        # while left < right:
+        #     nums[left], nums[right] = nums[right], nums[left]
+        #     left += 1
+        #     right -= 1
 
 
             

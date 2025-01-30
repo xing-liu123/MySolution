d1abf20983dcdf47e0f3403bff6493de7520a059
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-01-30 01:15:45 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,21 +1,25 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m = len(word1)
-        n = len(word2)
-
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-
+        m, n = len(word1), len(word2)
+        dp = [[sys.maxsize] * (n + 1) for _ in range(m + 1)]
         for i in range(m + 1):
             dp[i][0] = i
-
+        
         for j in range(n + 1):
             dp[0][j] = j
+
+        for i in range(1, min(m + 1, n + 1)):
+            if word1[i - 1] == word2[i - 1]:
+                dp[i][i] = dp[i - 1][i - 1]
+            else:
+                dp[i][i] = dp[i - 1][i - 1] + 1
 
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if word1[i - 1] == word2[j - 1]:
-                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j] + 1)
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j])
                 else:
-                    dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1)
+                    dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]) + 1, dp[i][j])
 
         return dp[m][n]
+                

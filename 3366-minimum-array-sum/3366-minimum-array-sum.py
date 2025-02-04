9aada283f555a45de38bd0cda89f81e5377ad471
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-04 02:10:49 -0500

    Time: 1827 ms (67.57%), Space: 37.2 MB (88.85%) - LeetHub

--- 3366-minimum-array-sum/3366-minimum-array-sum.py
+++ 3366-minimum-array-sum/3366-minimum-array-sum.py
@@ -1,52 +1,47 @@
 class Solution:
     def minArraySum(self, nums: List[int], k: int, op1: int, op2: int) -> int:
         n = len(nums)
-        # dp[i][j][l] represents minimum sum up to index i using j op1 operations and l op2 operations
-        dp = [[[float('inf')] * (op2 + 1) for _ in range(op1 + 1)] for _ in range(n)]
+        dp = [[[sys.maxsize] * (op2 + 1) for _ in range((op1 + 1))] for _ in range(n)]
+
         
-        # Initialize for first element
-        dp[0][0][0] = nums[0]  # No operations
+        dp[0][0][0] = nums[0]
+            
         if op1 > 0:
-            dp[0][1][0] = (nums[0] + 1) // 2  # Apply op1
+            dp[0][1][0] = (nums[0] + 1) // 2
+
         if op2 > 0 and nums[0] >= k:
-            dp[0][0][1] = nums[0] - k  # Apply op2
+            dp[0][0][1] = nums[0] - k
+
         if op1 > 0 and op2 > 0 and nums[0] >= k:
-            # Apply both operations - need to try both orders
-            val1 = ((nums[0] - k) + 1) // 2  # op2 then op1
-            val2 = ((nums[0] + 1) // 2) - k if (nums[0] + 1) // 2 >= k else float('inf')  # op1 then op2
+            val1 = (nums[0] + 1) // 2 - k if (nums[0] + 1) // 2 >= k else sys.maxsize
+            val2 = (nums[0] + 1 - k) // 2
             dp[0][1][1] = min(val1, val2)
-        
-        # Process rest of the array
+
+        res = sys.maxsize
+
         for i in range(1, n):
-            for j in range(op1 + 1):
-                for l in range(op2 + 1):
-                    # Try not applying any operation
-                    if dp[i-1][j][l] != float('inf'):
-                        dp[i][j][l] = min(dp[i][j][l], dp[i-1][j][l] + nums[i])
-                    
-                    # Try applying op1 (division)
-                    if j > 0 and dp[i-1][j-1][l] != float('inf'):
-                        dp[i][j][l] = min(dp[i][j][l], dp[i-1][j-1][l] + (nums[i] + 1) // 2)
-                    
-                    # Try applying op2 (subtraction)
-                    if l > 0 and nums[i] >= k and dp[i-1][j][l-1] != float('inf'):
-                        dp[i][j][l] = min(dp[i][j][l], dp[i-1][j][l-1] + nums[i] - k)
-                    
-                    # Try applying both operations
-                    if j > 0 and l > 0 and nums[i] >= k and dp[i-1][j-1][l-1] != float('inf'):
-                        # Try both orders of operations
-                        val1 = ((nums[i] - k) + 1) // 2  # op2 then op1
-                        val2 = ((nums[i] + 1) // 2) - k if (nums[i] + 1) // 2 >= k else float('inf')  # op1 then op2
-                        min_val = min(val1, val2)
-                        dp[i][j][l] = min(dp[i][j][l], dp[i-1][j-1][l-1] + min_val)
-        
-        # Find minimum sum in last row
-        result = float('inf')
+            for j in range(min(op1 + 1, i + 2)):
+                for l in range(min(op2 + 1, i + 2)):
+                    if dp[i - 1][j][l] != sys.maxsize:
+                        dp[i][j][l] = dp[i - 1][j][l] + nums[i]
+
+                    if j > 0 and dp[i - 1][j - 1][l] != sys.maxsize:
+                        dp[i][j][l] = min(dp[i][j][l], dp[i - 1][j - 1][l] + (nums[i] + 1) // 2)
+
+                    if l > 0 and dp[i - 1][j][l - 1] != sys.maxsize and nums[i] >= k:
+                        dp[i][j][l] = min(dp[i][j][l], dp[i - 1][j][l - 1] + (nums[i] - k))
+
+                    if j > 0 and l > 0 and dp[i - 1][j - 1][l - 1] != sys.maxsize and nums[i] >= k:
+                        val1 = dp[i - 1][j - 1][l - 1] + (nums[i] - k + 1) // 2
+                        val2 = dp[i - 1][j - 1][l - 1] + (nums[i] + 1) // 2 - k if (nums[i] + 1) // 2 >= k else sys.maxsize
+                        dp[i][j][l] = min(dp[i][j][l], val1, val2)
+  
+
         for j in range(op1 + 1):
             for l in range(op2 + 1):
-                result = min(result, dp[n-1][j][l])
-                
-        return result
+                res = min(res, dp[-1][j][l])
+
+        return res
 
 
 

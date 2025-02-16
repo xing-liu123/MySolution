63c0f1ab907a937628d9bd51686bb5709c590a52
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-15 21:04:11 -0500

    Time: 0 ms (100%), Space: 18.1 MB (23.18%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,47 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        path = []
+        # path = []
+        # res = []
+        # if len(digits) == 0:
+        #     return res
+
+        # key_map = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]] 
+
+        # def backtrack(idx):
+        #     if len(path) == len(digits):
+        #         res.append(''.join(path))
+        #         return
+
+        #     keys = key_map[ord(digits[idx]) - ord('2')]
+
+        #     for key in keys:
+        #         path.append(key)
+        #         backtrack(idx + 1)
+        #         path.pop()
+
+        # backtrack(0)
+        # return res
+
+        key_map = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]]
+
+        comb = []
         res = []
+
         if len(digits) == 0:
             return res
 
-        key_map = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]] 
-
-        def backtrack(idx):
-            if len(path) == len(digits):
-                res.append(''.join(path))
+        def backtracking(idx):
+            if idx == len(digits):
+                res.append(''.join(comb))
                 return
 
-            keys = key_map[ord(digits[idx]) - ord('2')]
+            chars = key_map[ord(digits[idx]) - ord('2')]
 
-            for key in keys:
-                path.append(key)
-                backtrack(idx + 1)
-                path.pop()
+            for char in chars:
+                comb.append(char)
+                backtracking(idx + 1)
+                comb.pop()
 
-        backtrack(0)
+        backtracking(0)
+
         return res

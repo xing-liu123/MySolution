20c3ae01c58e02213755294ae58e42467f3543f6
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-16 01:09:33 -0500

    Time: 0 ms (100%), Space: 17.8 MB (92.37%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,21 +1,42 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        path = [""] * (2 * n)
+        # path = [""] * (2 * n)
+        # res = []
+
+        # def generate(idx, open_count, close_count):
+        #     if idx == 2 * n:
+        #         res.append("".join(path))
+        #         return
+
+        #     if open_count < n:
+        #         path[idx] = "("
+        #         generate(idx + 1, open_count + 1, close_count)
+
+        #     if close_count < open_count:
+        #         path[idx] = ")"
+        #         generate(idx + 1, open_count, close_count + 1)
+        
+        # generate(0, 0, 0)
+
+        # return res
+
+        comb = [""] * (n * 2)
+
         res = []
 
-        def generate(idx, open_count, close_count):
+        def generate(idx, openCount, closeCount):
             if idx == 2 * n:
-                res.append("".join(path))
+                res.append("".join(comb))
                 return
 
-            if open_count < n:
-                path[idx] = "("
-                generate(idx + 1, open_count + 1, close_count)
+            if openCount < n:
+                comb[idx] = "("
+                generate(idx + 1, openCount + 1, closeCount)
+            
+            if closeCount < openCount:
+                comb[idx] = ")"
+                generate(idx + 1, openCount, closeCount + 1)
 
-            if close_count < open_count:
-                path[idx] = ")"
-                generate(idx + 1, open_count, close_count + 1)
-        
         generate(0, 0, 0)
 
         return res

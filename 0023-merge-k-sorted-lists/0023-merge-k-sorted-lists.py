c1ec549f71db08e9bcae1dbcd8f128aeff40b11b
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-17 14:44:16 -0500

    Time: 6 ms (93.26%), Space: 20.5 MB (19.3%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,44 +3,62 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+from itertools import count
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        print(len(lists))
-        if len(lists) == 0:
-            return None
-        elif len(lists) == 1:
-            return lists[0]
+        # if len(lists) == 0:
+        #     return None
+        # elif len(lists) == 1:
+        #     return lists[0]
         
 
-        mid = len(lists) // 2
+        # mid = len(lists) // 2
 
-        left = self.mergeKLists(lists[:mid])
-        right = self.mergeKLists(lists[mid:])
+        # left = self.mergeKLists(lists[:mid])
+        # right = self.mergeKLists(lists[mid:])
 
-        def merge(node1, node2):
-            dummy = ListNode()
-            curr = dummy
+        # def merge(node1, node2):
+        #     dummy = ListNode()
+        #     curr = dummy
 
-            while node1 or node2:
-                if not node1:
-                    curr.next = node2
-                    break
-                elif not node2:
-                    curr.next = node1
-                    break
-                else:
-                    if node1.val <= node2.val:
-                        curr.next = node1
-                        curr = curr.next
-                        node1 = node1.next
-                    else:
-                        curr.next = node2
-                        curr = curr.next
-                        node2 = node2.next
+        #     while node1 or node2:
+        #         if not node1:
+        #             curr.next = node2
+        #             break
+        #         elif not node2:
+        #             curr.next = node1
+        #             break
+        #         else:
+        #             if node1.val <= node2.val:
+        #                 curr.next = node1
+        #                 curr = curr.next
+        #                 node1 = node1.next
+        #             else:
+        #                 curr.next = node2
+        #                 curr = curr.next
+        #                 node2 = node2.next
 
-            return dummy.next
+        #     return dummy.next
 
-        return merge(left, right)
+        # return merge(left, right)
+        counter = count()
+        heap = [(node.val, next(counter), node) for node in lists if node]
+        heapq.heapify(heap)
+        dummy = ListNode()
+        curr = dummy
+
+        while heap:
+            _, _, node = heapq.heappop(heap)
+            curr.next = node
+            curr = curr.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, next(counter), node.next))
+
+        return dummy.next
+
                 
 
 

08c5cddf9c0a0ea4fce5c014f3828bf9d819b5d8
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-25 17:51:31 -0500

    Time: 0 ms (100%), Space: 17.7 MB (74.77%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -2,31 +2,56 @@ import heapq
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        counter = Counter(s) # Counts the frequency of each character
+        # # O(N), where N is the length of s
+        # counter = Counter(s) # Counts the frequency of each character
 
-        maxHeap = [(-freq, char) for char, freq in counter.items()]
+        # # O(M), where M is the number of unique characters
+        # maxHeap = [(-freq, char) for char, freq in counter.items()] 
 
-        heapq.heapify(maxHeap)
+        # # O(M), where M is the number of unique characters. Downheap operation.
+        # heapq.heapify(maxHeap)
 
-        lastChar = None
+        # lastChar = None # Use it to hold the lastest appended character to avoid the same character being ajacent
 
-        res = ""
+        # res = ""
 
-        while maxHeap:
-            freq, char = heapq.heappop(maxHeap)
+        # # Nlog(M), there are at most M elements in the heap, pop taks log(M), and we need pop at most N times.
+        # while maxHeap:
+        #     freq, char = heapq.heappop(maxHeap)
 
-            res += char
+        #     res += char
+            
+        #     # Put the character back to the heap after one iteraton
+        #     if lastChar:
+        #         heapq.heappush(maxHeap, lastChar)
+        #         lastChar = None
 
-            if lastChar:
-                heapq.heappush(maxHeap, lastChar)
-                lastChar = None
+        #     freq += 1
+        #     # Store it in lastChar to avoid being ajacent
+        #     if freq < 0:
+        #         lastChar = (freq, char)
 
-            freq += 1
-            if freq < 0:
-                lastChar = (freq, char)
+        # # If there are characters leftover, it is impossible to rerange.
+        # if lastChar:
+        #     return ""
 
-        if lastChar:
+        # # Overall runtime, O(Nlog(M)), Space complexity: O(M) for counter and O(M) for heap, where M is number of unique characters
+        # return res
+        counter = Counter(s)
+        maxFreq = max(counter.values())
+        if maxFreq > (len(s) + 1) // 2:
             return ""
-
-        return res
+        
+        sortedChars = sorted(counter.items(), key=lambda x: -x[1])
+        res = [None] * len(s)
+        
+        index = 0
+        for char, freq in sortedChars:
+            for _ in range(freq):
+                res[index] = char
+                index += 2
+                if index >= len(s):
+                    index = 1
+        
+        return ''.join(res)
 

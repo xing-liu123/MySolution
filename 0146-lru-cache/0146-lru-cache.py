631c4f2be50e93d2158ba6f798a67c286f490f28
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-25 15:36:23 -0500

    Time: 96 ms (94.86%), Space: 78.6 MB (46.32%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,9 +1,11 @@
 class Node:
-    def __init__(self, key=None, val=None):
+
+    def __init__(self, key = -1, val = -1):
         self.key = key
         self.val = val
-        self.prev = None
         self.next = None
+        self.prev = None
+
 
 class LRUCache:
 
@@ -15,43 +17,42 @@ class LRUCache:
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def removeNode(self, node):
+    def _remove(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
 
     def addToFront(self, node):
+        node.prev = self.head
         node.next = self.head.next
         self.head.next.prev = node
         self.head.next = node
-        node.prev = self.head
 
     def get(self, key: int) -> int:
         if not key in self.cache:
             return -1
 
         node = self.cache[key]
-        self.removeNode(node)
+        self._remove(node)
         self.addToFront(node)
-
         return node.val
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
             node.val = value
-            self.removeNode(node)
+            self._remove(node)
             self.addToFront(node)
+
         else:
-            node = Node(key, value)
+            if len(self.cache) == self.capacity:
+                node = self.tail.prev
+                self._remove(node)
+                del self.cache[node.key]
 
-            if len(self.cache) >= self.capacity:
-                nodeToRemove = self.tail.prev
-                self.removeNode(nodeToRemove)
-                del self.cache[nodeToRemove.key]
-
-            self.cache[key] = node
-            self.addToFront(node)
-        
+            newNode = Node(key, value)
+            self.cache[key] = newNode
+            self.addToFront(newNode)
+            
 
 
 # Your LRUCache object will be instantiated and called as such:

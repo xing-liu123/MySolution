be499fef2aff0f87861dfce3a67c070b9d8ad513
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-25 14:52:51 -0500

    Time: 289 ms (52.56%), Space: 18.7 MB (85.01%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,34 +1,65 @@
 from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        word_set = set(wordList)
+        # word_set = set(wordList)
 
-        if not endWord in word_set:
+        # if not endWord in word_set:
+        #     return 0
+
+        # queue = deque([beginWord])
+
+        # length = 1
+
+        # while queue:
+        #     size = len(queue)
+        #     length += 1
+
+        #     for _ in range(size):
+        #         word = queue.popleft()
+
+        #         for i in range(len(word)):
+        #             for j in range(26):
+        #                 next_word = word[:i] + chr(ord('a') + j) + word[i + 1:]
+
+        #                 if next_word in word_set:
+        #                     if next_word == endWord:
+        #                         return length
+
+        #                     queue.append(next_word)
+        #                     word_set.remove(next_word)
+        # return 0
+
+        from collections import deque
+        
+        wordSet = set(wordList)
+
+        if not endWord in wordSet:
             return 0
 
         queue = deque([beginWord])
-
-        length = 1
+        count = 0
 
         while queue:
             size = len(queue)
-            length += 1
+            count += 1
 
             for _ in range(size):
                 word = queue.popleft()
 
                 for i in range(len(word)):
-                    for j in range(26):
-                        next_word = word[:i] + chr(ord('a') + j) + word[i + 1:]
+                    for c in string.ascii_lowercase:
+                        nextWord = word[:i] + c + word[i + 1:]
 
-                        if next_word in word_set:
-                            if next_word == endWord:
-                                return length
+                        if nextWord in wordSet:
+                            if nextWord == endWord:
+                                return count + 1
 
-                            queue.append(next_word)
-                            word_set.remove(next_word)
-            print(queue)
+                            queue.append(nextWord)
+                            wordSet.remove(nextWord)
+
         return 0
-                        
+
+
+        
 
             

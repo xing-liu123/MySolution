4cde1aee9f16fc0295593bc1dcc96a52098ce9ab
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-25 22:48:58 -0500

    Time: 48 ms (15.06%), Space: 18.7 MB (41.85%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -6,21 +6,20 @@ class Node:
         self.next = next
         self.random = random
 """
-
+node_map = {}
 class Solution:
-    def __init__(self):
-        self.node_map = {}
-    
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
 
-        if head in self.node_map:
-            return self.node_map[head]
-        
-        copy = Node(head.val)
-        self.node_map[head] = copy
-        copy.next = self.copyRandomList(head.next)
-        copy.random = self.copyRandomList(head.random)
+        if head in node_map:
+            return node_map[head]
 
-        return copy
+        node = Node(head.val)
+        node_map[head] = node
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+        
+
+        return node
+

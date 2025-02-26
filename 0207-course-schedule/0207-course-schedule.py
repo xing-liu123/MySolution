2839021679ecf6190d15b5b5e4b2a076a40a64a3
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-26 14:51:56 -0500

    Time: 4 ms (70.39%), Space: 20.1 MB (8.39%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,28 +1,62 @@
 from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        course_map = defaultdict(list)
-        indegree = [0] * numCourses
+        # course_map = defaultdict(list)
+        # indegree = [0] * numCourses
+
+        # for course, preq in prerequisites:
+        #     course_map[preq].append(course)
+        #     indegree[course] += 1
+
+        # queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+
+        # visited = 0
+
+        # while queue:
+        #     course = queue.popleft()
+        #     visited += 1
+
+        #     for next_course in course_map[course]:
+        #         indegree[next_course] -= 1
+        #         if indegree[next_course] == 0:
+        #             queue.append(next_course)
+
+        # return visited == numCourses
+        
+        graph = defaultdict(list)
 
         for course, preq in prerequisites:
-            course_map[preq].append(course)
-            indegree[course] += 1
+            graph[preq].append(course)
 
-        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        visited = [0] * numCourses
+        visitedCount = 0
 
-        visited = 0
+        hasCycle = False
 
-        while queue:
-            course = queue.popleft()
-            visited += 1
+        def dfs(curr):
+            nonlocal visitedCount, hasCycle
+            if visited[curr] == 1:
+                hasCycle = True
+                return
 
-            for next_course in course_map[course]:
-                indegree[next_course] -= 1
-                if indegree[next_course] == 0:
-                    queue.append(next_course)
+            if visited[curr] == 2:
+                return
 
-        return visited == numCourses
-            
+            visited[curr] = 1
+
+            for nextCourse in graph[curr]:
+                dfs(nextCourse)
+
+            visited[curr] = 2
+            visitedCount += 1
+
+        for i in range(numCourses):
+            dfs(i)
+
+        return not hasCycle and visitedCount == numCourses
+
+
+        
 
 
 

8eb9a6f3c0c11161e039d79f288bb4d1d580589f
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-25 20:12:55 -0500

    Time: 11 ms (24.59%), Space: 21.4 MB (40.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -43,7 +43,7 @@ class Solution:
             
         n = len(freq_list)
 
-        quickSelect(0, n - 1, k)
+        quickSelect(0, n - 1, k - 1)
 
         return [num for num, _ in freq_list[:k]]
 

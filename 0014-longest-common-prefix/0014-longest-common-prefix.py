10343732c978d11076a24e673c6149f1f731c55e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-26 19:57:32 -0500

    Time: 3 ms (28.55%), Space: 18.1 MB (8.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,20 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+        
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        self.prefix = None
+    
+    def insert(self, word):
+        curr = self.root
+        currPrefix = ""
+
+        for c in word:
+            if not c in curr.children:
+                curr.children[c] = TrieNode()
+
+            if len(curr.children) > 1:
+                break
+            currPrefix += c
+            curr = curr.children[c]
+
+        if not self.prefix or len(currPrefix) < len(self.prefix):
+            self.prefix = currPrefix
+
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        res = ""
+        root = Trie()
+        
+        for s in strs:
+            if s == "":
+                return ""
+            root.insert(s)
 
-        idx = 0
+        return root.prefix
 
-        while True:
-            if idx >= len(strs[0]):
-                return res
+        
             
-            char = strs[0][idx]
 
-            for s in strs:
-                if idx >= len(s) or s[idx] != char:
-                    return res
-            
-            idx += 1
-            res += char
-
-        return res

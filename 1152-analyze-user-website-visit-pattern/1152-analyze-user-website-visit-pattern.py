cf6d61d49c0c9dbe0f54035e380b03a8c4560ed4
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-02-26 19:33:43 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -5,33 +5,34 @@ class Visit:
 
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        user_visit = defaultdict(list)
+        userVisitedLists = defaultdict(list)
 
         for user, time, web in zip(username, timestamp, website):
-            user_visit[user].append((time, web))
+            userVisitedLists[user].append((time, web))
 
-        pattern_count = Counter()
+        patternCount = defaultdict(int)
 
-        for visit_list in user_visit.values():
-            visit_list.sort()
-            pattern_set = set()
+        for visitList in userVisitedLists.values():
+            if len(visitList) >= 3:
+                visitList.sort()
+                uniquePatterns = set()
+                for i in range(len(visitList) - 2):
+                    _, web1 = visitList[i]
+                    _, web2 = visitList[i + 1]
+                    _, web3 = visitList[i + 2]
+                    uniquePatterns.add((web1, web2, web3))
 
-            n = len(visit_list)
-
-            for i in range(n):
-                for j in range(i + 1, n):
-                    for k in range(j + 1, n):
-                        pattern = (visit_list[i][1], visit_list[j][1], visit_list[k][1])
-                        if not pattern in pattern_set:
-                            pattern_set.add(pattern)
-                            pattern_count[pattern] += 1
-            
-        sorted_pattern = sorted(pattern_count.items(), key=lambda x: (-x[1], x[0]))
-
-        return list(sorted_pattern[0][0])
-
-                
-
+                for uniquePattern in uniquePatterns:
+                    patternCount[uniquePattern] += 1
         
+        maxCount = max(patternCount.values())
+
+        return sorted(triple for triple, count in patternCount.items() if count == maxCount)[0]
+
+
+
+
+
+
 
         

0aeefbb0dee55c18211f6ffd65b14f110ee8454c
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-02 13:36:16 -0500

    Time: 6 ms (83.21%), Space: 19.1 MB (50.62%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,5 +1,18 @@
+from heapq import heappush, heappop
 class Solution:
     def frequencySort(self, s: str) -> str:
         counts = Counter(s)
 
-        return "".join(sorted(list(s), key=lambda x: (counts[x], x), reverse=True))
+        maxHeap = []
+
+        for char, count in counts.items():
+            heappush(maxHeap, (-count, char))
+
+        res = ""
+
+        while maxHeap:
+            count, char = heappop(maxHeap)
+
+            res += char * (-count)
+
+        return res

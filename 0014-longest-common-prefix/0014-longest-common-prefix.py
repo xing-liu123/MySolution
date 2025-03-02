7a675a305b509dd6deac0469cb88feae9cc29c9e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-01 23:58:56 -0500

    Time: 3 ms (28.4%), Space: 17.9 MB (30.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,41 +1,57 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        # self.is_end = False
+# class TrieNode:
+#     def __init__(self):
+#         self.children = {}
+#         # self.is_end = False
         
 
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-        self.prefix = None
+# class Trie:
+#     def __init__(self):
+#         self.root = TrieNode()
+#         self.prefix = None
     
-    def insert(self, word):
-        curr = self.root
-        currPrefix = ""
+#     def insert(self, word):
+#         curr = self.root
+#         currPrefix = ""
 
-        for c in word:
-            if not c in curr.children:
-                curr.children[c] = TrieNode()
+#         for c in word:
+#             if not c in curr.children:
+#                 curr.children[c] = TrieNode()
 
-            if len(curr.children) > 1:
-                break
-            currPrefix += c
-            curr = curr.children[c]
+#             if len(curr.children) > 1:
+#                 break
+#             currPrefix += c
+#             curr = curr.children[c]
 
-        if not self.prefix or len(currPrefix) < len(self.prefix):
-            self.prefix = currPrefix
+#         if not self.prefix or len(currPrefix) < len(self.prefix):
+#             self.prefix = currPrefix
 
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        root = Trie()
+        # root = Trie()
         
-        for s in strs:
-            if s == "":
-                return ""
-            root.insert(s)
+        # for s in strs:
+        #     if s == "":
+        #         return ""
+        #     root.insert(s)
 
-        return root.prefix
+        # return root.prefix
+
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for word in strs[1:]:
+                if i == len(word) or word[i] != c:
+                    if i == 0:
+                        return ""
+                    else:
+                        return strs[0][:i]
+
+
+        return strs[0]
+            
+
+
+
 
         
             

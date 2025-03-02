2e53e978174889d7e3d1f2b9fe8df20fbd52cc63
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-02 00:12:45 -0500

    Time: 0 ms (100%), Space: 17.7 MB (88.25%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -3,42 +3,35 @@ class Solution:
         res = []
 
         m, n = len(matrix), len(matrix[0])
-
-        size = m * n
         row, col = 0, 0
-        direction = 0 if n > 1 else 1 # 0: right, 1: down, 2: left, 3: right
-        top, left, bottom, right = 1, 0, m - 1, n - 1
 
-        while len(res) < size:
-            print(row, col)
+        top, bottom, left, right = 1, m - 1, 0, n - 1
+        currDir = 0 if n > 1 else 1 # 0: right, 1: down, 2: left, 3: up
+
+        for _ in range(m * n):
             res.append(matrix[row][col])
 
-            if direction == 0:
-                col += 1
+            match currDir:
+                case 0:
+                    col += 1
+                    if col == right:
+                        currDir = 1
+                        right -= 1
 
-                if col == right:
-                    direction = 1
-                    right -= 1
-
-            elif direction == 1:
-                row += 1
-
-                if row == bottom:
-                    direction = 2
-                    bottom -= 1
-
-            elif direction == 2:
-                col -= 1
-
-                if col == left:
-                    direction = 3
-                    left += 1
-            else:
-                row -= 1
-
-                if row == top:
-                    direction = 0
-                    top += 1
+                case 1:
+                    row += 1
+                    if row == bottom:
+                        currDir = 2
+                        bottom -= 1
+                case 2:
+                    col -= 1
+                    if col == left:
+                        currDir = 3
+                        left += 1
+                case 3:
+                    row -= 1
+                    if row == top:
+                        currDir = 0
+                        top += 1
 
         return res
-

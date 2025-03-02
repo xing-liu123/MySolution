d62e6491a7f86f3d09bd220f1b632c1801cfc51e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-02 00:51:49 -0500

    Time: 5665 ms (5.01%), Space: 32.9 MB (19.25%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,40 +1,35 @@
-from collections import deque
-class MyQueue:
+class MaxQueue:
     def __init__(self):
-        self.queue = deque()
+        self.queue = []
 
-    def push(self, val):
-        if not self.queue:
-            self.queue.append(val)
-        else:
-            while self.queue and val > self.queue[-1]:
-                self.queue.pop()
+    def enque(self, val):
+        while self.queue and val > self.queue[-1]:
+            self.queue.pop()
 
-            self.queue.append(val)
-    
+        self.queue.append(val)
+
     def remove(self, val):
-        if self.queue[0] == val:
-            self.queue.popleft()
+        if val == self.queue[0]:
+            self.queue = self.queue[1:]
 
-    def front(self):
-        return self.queue[0]
-
+    def getMax(self):
+        if len(self.queue) > 0:
+            return self.queue[0]
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        queue = MyQueue()
+        queue = MaxQueue()
+
         res = []
 
-        for i in range(len(nums)):
-            queue.push(nums[i])
+        for idx, num in enumerate(nums):
+            queue.enque(num)
 
-            if i >= k:
-                queue.remove(nums[i - k])
+            if idx >= k:
+                queue.remove(nums[idx - k])
 
-            if i >= k - 1:
-                res.append(queue.front())
+            if idx >= k - 1:
+                res.append(queue.getMax())
 
         return res
             
-            
-

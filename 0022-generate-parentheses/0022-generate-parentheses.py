d8b2e8623a0ca7270747dee0bca57b9c40ca196e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-02 10:46:37 -0500

    Time: 0 ms (100%), Space: 18 MB (43.89%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,42 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # path = [""] * (2 * n)
-        # res = []
-
-        # def generate(idx, open_count, close_count):
-        #     if idx == 2 * n:
-        #         res.append("".join(path))
-        #         return
-
-        #     if open_count < n:
-        #         path[idx] = "("
-        #         generate(idx + 1, open_count + 1, close_count)
-
-        #     if close_count < open_count:
-        #         path[idx] = ")"
-        #         generate(idx + 1, open_count, close_count + 1)
-        
-        # generate(0, 0, 0)
-
-        # return res
-
         comb = [""] * (n * 2)
-
         res = []
 
-        def generate(idx, openCount, closeCount):
+        def backtracking(idx, openCount, closeCount):
             if idx == 2 * n:
-                res.append("".join(comb))
+                res.append(''.join(comb))
                 return
 
             if openCount < n:
                 comb[idx] = "("
-                generate(idx + 1, openCount + 1, closeCount)
-            
+                backtracking(idx + 1, openCount + 1, closeCount)
+
             if closeCount < openCount:
                 comb[idx] = ")"
-                generate(idx + 1, openCount, closeCount + 1)
+                backtracking(idx + 1, openCount, closeCount + 1)
 
-        generate(0, 0, 0)
+        backtracking(0, 0, 0)
 
         return res

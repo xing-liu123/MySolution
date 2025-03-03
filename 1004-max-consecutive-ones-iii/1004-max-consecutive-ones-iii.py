afc12ed8fb9a7b8fe798f1c2ea2136f5a8fec975
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-03 01:55:52 -0500

    Time: 65 ms (56.74%), Space: 18.2 MB (81.48%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,20 +1,37 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        zeroCountIdx = {}
-        zeroCountIdx[0] = -1
+        # zeroCountIdx = {}
+        # zeroCountIdx[0] = -1
+        # zeroCount = 0
+        # maxLength = 0
+
+        # for idx, num in enumerate(nums):
+        #     if num == 0:
+        #         zeroCount += 1
+
+        #     left = max(0, zeroCount - k)
+
+        #     if left in zeroCountIdx:
+        #         maxLength = max(maxLength, idx - zeroCountIdx[left])
+
+        #     if not zeroCount in zeroCountIdx:
+        #         zeroCountIdx[zeroCount] = idx
+
+        # return maxLength
         zeroCount = 0
+        left = 0
+
         maxLength = 0
 
-        for idx, num in enumerate(nums):
+        for right, num in enumerate(nums):
             if num == 0:
                 zeroCount += 1
 
-            left = max(0, zeroCount - k)
+            while zeroCount > k:
+                if nums[left] == 0:
+                    zeroCount -= 1
+                left += 1
 
-            if left in zeroCountIdx:
-                maxLength = max(maxLength, idx - zeroCountIdx[left])
-
-            if not zeroCount in zeroCountIdx:
-                zeroCountIdx[zeroCount] = idx
+            maxLength = max(maxLength, right - left + 1)
 
         return maxLength

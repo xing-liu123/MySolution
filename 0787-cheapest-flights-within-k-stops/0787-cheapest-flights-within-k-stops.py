9c8196c694d62657fdf6c622a6cab6e65a6f929e
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-04 01:47:42 -0500

    Time: 31 ms (41.82%), Space: 19.6 MB (17.37%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -2,30 +2,28 @@ class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = defaultdict(list)
 
-        for u, v, p in flights:
-            graph[u].append((v, p))
-        
-        min_heap = [(0, src, 0)]
-        price_map = dict()
-        price_map[src] = (0, 0)
+        for source, dist, price in flights:
+            graph[source].append((dist, price))
 
-        while min_heap:
-            cost, city, stop = heapq.heappop(min_heap)
-            if city == dst and stop - 1 <= k:
-                return cost
+        heap = [(0, src, 0)]
+        priceAtStop = {}
+
+        while heap:
+            totalPrice, curr, stop = heapq.heappop(heap)
+
+            if curr == dst:
+                return totalPrice
 
             if stop > k:
                 continue
 
-            for next_city, next_cost in graph[city]:
-                if not next_city in price_map:
-                    heapq.heappush(min_heap, (cost + next_cost, next_city, stop + 1))
-                    price_map[next_city] = (cost + next_cost, stop + 1)
-                else:
-                    p, s = price_map[next_city]
-                    if cost + next_cost < p or stop + 1 < s:
-                        heapq.heappush(min_heap, (cost + next_cost, next_city, stop + 1))
-                        price_map[next_city] = (cost + next_cost, stop + 1)
+            for neighbor, price in graph[curr]:
+                newPrice = totalPrice + price
 
-                    
+                if not (neighbor, stop) in priceAtStop or newPrice < priceAtStop[(neighbor, stop)]:
+                    priceAtStop[(neighbor, stop)] = newPrice
+                    heapq.heappush(heap, (newPrice, neighbor, stop + 1))
+
         return -1
+
+

98e3d0a1bbe7448f0dc1b49df86a83c4dd659031
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-04 12:55:01 -0500

    Time: 3 ms (25.55%), Space: 18.1 MB (8.24%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -13,7 +13,13 @@ class Solution:
 
         if str1 + str2 != str2 + str1:
             return ""
+        
+        def gcd(a, b):
+            if b == 0:
+                return a
+            
+            return gcd(b, a % b)
 
-        gcd_len = math.gcd(len(str1), len(str2))
+        gcd_len = gcd(len(str1), len(str2))
 
         return str1[:gcd_len]

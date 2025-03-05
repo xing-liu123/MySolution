9ebb243a2105b882fc388311e02fbac7d8ec44bb
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-04 22:10:37 -0500

    Time: 7 ms (40.1%), Space: 17.7 MB (94.79%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -2,11 +2,12 @@ class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
         stack = []
         partLen = len(part)
+        partList = list(part)
 
         for c in s:
             stack.append(c)
 
-            if len(stack) >= len(part) and ''.join(stack[-partLen:]) == part:
+            if len(stack) >= len(part) and stack[-partLen:] == partList:
                 stack = stack[:-partLen]
             
         return "".join(stack)

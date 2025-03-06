e8eb14c76a22bdbf7ce61713eecd390ca4d634d6
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-03-06 10:43:16 -0500

    Time: 0 ms (100%), Space: 18.2 MB (8.97%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -6,30 +6,52 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
+        # if not root:
+        #     return []
 
-        queue = deque([root])
+        # queue = deque([root])
+        # res = []
+
+        # while queue:
+        #     size = len(queue)
+        #     nodes = []
+
+        #     for _ in range(size):
+        #         node = queue.popleft()
+
+        #         if node.left:
+        #             queue.append(node.left)
+
+        #         if node.right:
+        #             queue.append(node.right)
+
+        #         nodes.append(node.val)
+
+        #     if len(res) % 2 == 0:
+        #         res.append(nodes)
+        #     else:
+        #         res.append(nodes[::-1])
+
+        # return res
         res = []
 
-        while queue:
-            size = len(queue)
-            nodes = []
+        def dfs(curr, level):
+            if not curr:
+                return 
 
-            for _ in range(size):
-                node = queue.popleft()
-
-                if node.left:
-                    queue.append(node.left)
-
-                if node.right:
-                    queue.append(node.right)
-
-                nodes.append(node.val)
-
-            if len(res) % 2 == 0:
-                res.append(nodes)
+            if level == len(res):
+                res.append([])
+            
+            if level % 2 == 0:
+                res[level].append(curr.val)
             else:
-                res.append(nodes[::-1])
+                res[level].insert( 0, curr.val)
+
+            dfs(curr.left, level + 1)
+            dfs(curr.right, level + 1)
+
+        dfs(root, 0)
 
         return res
+
+            

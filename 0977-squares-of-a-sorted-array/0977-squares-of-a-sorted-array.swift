1961eaf14ae8cd0916385e26a5115a9088a9df99
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-05-10 23:26:12 -0400

    Time: 0 ms (100%), Space: 21.7 MB (6.88%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.swift
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.swift
@@ -1,12 +1,28 @@
 class Solution {
     func sortedSquares(_ nums: [Int]) -> [Int] {
-        var res : [Int] = []
+        var res = [Int](repeating: 0, count: nums.count)
 
-        for num in nums {
-            res.append(num * num)
+        var idx = nums.count - 1
+        var left = 0
+        var right = nums.count - 1
+
+        while left <= right {
+            let val1 = nums[left] * nums[left]
+            let val2 = nums[right] * nums[right]
+
+            if val1 >= val2 {
+                res[idx] = val1
+                left += 1
+            } else {
+                res[idx] = val2
+                right -= 1
+            }
+
+            idx -= 1
         }
 
-        res.sort()
+        return res
+        
 
         return res
     }

865548a6f32139d113c5b82addd07a3e2df2442d
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-08-24 21:55:14 -0700

    Time: 8 ms (37.12%), Space: 19.4 MB (50.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -7,9 +7,9 @@ class Solution:
             if count == 0:
                 candidate = num
                 count = 1
-            elif num == candidate:
+            elif candidate == num:
                 count += 1
             else:
                 count -= 1
-        
+
         return candidate

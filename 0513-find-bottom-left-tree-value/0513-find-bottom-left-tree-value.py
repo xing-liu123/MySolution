b2a2a03cb311f158805e85c3f1605e6c910cb033
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-08-25 22:16:39 -0700

    Time: 8 ms (6.49%), Space: 19.8 MB (94.07%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -1,4 +1,3 @@
-from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,39 +6,21 @@ from collections import deque
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-#         res = root.val
+        res = root.val  
         
-#         queue = deque([root])
+        queue = deque([root])
         
-#         while queue:
-#             size = len(queue)
-#             res = queue[0].val
+        while queue:
+            size = len(queue)
+            res = queue[0].val
             
-#             for _ in range(size):
-#                 curr = queue.popleft()
+            for _ in range(size):
+                curr = queue.popleft()
                 
-#                 if curr.left:
-#                     queue.append(curr.left)
+                if curr.left:
+                    queue.append(curr.left)
                     
-#                 if curr.right:
-#                     queue.append(curr.right)
+                if curr.right:
+                    queue.append(curr.right)
         
-#         return res
-        return self.findLeft(root, 0)[1]
-
-    def findLeft(self, curr, depth) -> (int, int):
-        if not curr.left and not curr.right:
-            return depth, curr.val
-        
-                
-        leftDepth, leftVal = self.findLeft(curr.left, depth + 1) if curr.left else (-1, 0)
-        
-        rightDepth, rightVal = self.findLeft(curr.right, depth + 1) if curr.right else (-1, 0)
-
-                
-        if leftDepth >= rightDepth:
-            return leftDepth, leftVal
-        else:
-            return rightDepth, rightVal
-        
-            
+        return res

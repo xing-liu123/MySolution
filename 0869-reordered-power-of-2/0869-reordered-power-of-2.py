0147109511d96cb14464226ab3ec348130025149
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-08-30 20:29:07 -0700

    Time: 7 ms (22.91%), Space: 17.8 MB (38.42%) - LeetHub

---
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.py
@@ -0,0 +1,23 @@
+from collections import Counter
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+
+
+        strN = str(n)
+        counts = Counter(strN)
+
+        curr = 1
+        countSet = set()
+
+        while len(str(curr)) <= len(strN):
+            currCounts = Counter(str(curr))
+
+            if counts == currCounts:
+                print(counts)
+                return True
+
+            curr *= 2
+
+        return False
+
+

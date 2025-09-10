1a212ae77f7942c3873a436cabd5bec74aee53dd
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-09-09 21:22:58 -0700

    Time: 7 ms (97.95%), Space: 20.5 MB (97.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,25 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list) # key is the sortedWord
- 
+        anagrams = defaultdict(list)
+
         for word in strs:
-            charCount = [0] * 26
+            sortedStr = ''.join(sorted(list(word)))
+            anagrams[sortedStr].append(word)
 
-            for c in word:
-                idx = ord(c) - ord('a')
-                charCount[idx] += 1
+        return [anagram for anagram in anagrams.values()]
+
+        # anagrams = defaultdict(list) # key is the sortedWord
+ 
+        # for word in strs:
+        #     charCount = [0] * 26
+
+        #     for c in word:
+        #         idx = ord(c) - ord('a')
+        #         charCount[idx] += 1
             
-            anagrams[tuple(charCount)].append(word)
+        #     anagrams[tuple(charCount)].append(word)
 
-        return list(anagrams.values())
+        # return list(anagrams.values())
             
 
         

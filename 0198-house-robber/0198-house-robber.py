5416dbe2422fd71d1320970b6509359b2959d0cd
Haru&Kaki <78183814+xing-liu123@users.noreply.github.com>
2025-09-15 20:35:38 -0700

    Time: 0 ms (100%), Space: 18 MB (12.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -12,6 +12,19 @@ class Solution:
 
         # return max(x, y) 
         
+        # if len(nums) == 1:
+        #     return nums[0]
+
+        # x = nums[0]
+        # y = max(nums[0], nums[1])
+
+        # for i in range(2, len(nums)):
+        #     z = y
+        #     y = max(y, x + nums[i])
+        #     x = z
+        
+        # return y
+
         if len(nums) == 1:
             return nums[0]
 
